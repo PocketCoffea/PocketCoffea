bf4a5ac810cbe7fb520de3fecb5e7abb96993d81
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2022-03-02 16:59:11 +0100

    Cuts as function

---
+++ lib/cuts.py
@@ -0,0 +1,18 @@
+def dilepton(processor, events):
+
+    if processor._year == '2017':
+        MET = events.METFixEE2017
+    else:
+        MET = events.MET
+
+    # Masks for same-flavor (SF) and opposite-sign (OS)
+    SF = ( ((events.nmuon == 2) & (events.nelectron == 0)) | ((events.nmuon == 0) & (events.nelectron == 2)) )
+    OS = (events.ll.charge == 0)
+    #SFOS = SF & OS
+    not_SF = ( (events.nmuon == 1) & (events.nelectron == 1) )
+
+    mask = ( (events.nlep == 2) & (events.nlepgood >= 1) & OS &
+             (events.njet >= 2) & (events.nbjet > 1) & (MET.pt > 40) &
+             (events.ll.mass > 20) & ((SF & ((events.ll.mass < 76) | (events.ll.mass > 106))) | not_SF) )
+
+    return mask
