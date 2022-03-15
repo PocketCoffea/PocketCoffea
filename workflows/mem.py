5db2c68affd78ae5898392e89461ea60c8e306c6
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2022-03-15 18:58:47 +0100

    Almost final version of parton matching

--- workflows/mem.py
+++ workflows/mem.py
@@ -1,3 +1,4 @@
+import sys
 import awkward as ak
 from coffea import hist
 
@@ -33,22 +34,41 @@ class MEMStudiesProcessor(ttHbbBaseProcessor):
         pairs_sorted = pairs[idx_pairs_sorted]
         idx_bquarks, idx_JetGood = ak.unzip(pairs_sorted)
 
-        hasMatch = ak.zeros_like(idx_JetGood)
-        for idx_bquark in range(Nbmax):
-        #for idx_pair in range(ak.max(ak.num(idx_bquarks))):
-            idx_bquark = idx_bquarks[idx_pair]
-            print(idx_bquark, self.events.metadata["dataset"], hasMatch)
-            print(idx_bquarks)
+        hasMatch = ak.zeros_like(idx_JetGood, dtype=bool)
+        Npairmax = ak.max(ak.num(idx_bquarks))
+        #for idx_bquark in range(Nbmax):
+        for idx_pair in range(Npairmax):
+            idx_bquark = ak.pad_none(idx_bquarks, Npairmax)[:,idx_pair]
+            print(idx_pair, self.events.metadata["dataset"], hasMatch, end='\n\n')
+            #print("idx_bquarks", idx_bquarks, end='\n\n')
+            #print("idx_bquark", idx_bquark, end='\n\n')
+
+            print("~hasMatch", ak.fill_none(~hasMatch, False))
+            print("(idx_bquarks == idx_bquark) & ~hasMatch", ak.fill_none( (idx_bquarks == idx_bquark) & ~hasMatch, False ), end='\n\n')
+
+            idx_match_candidates = idx_JetGood[ak.fill_none( (idx_bquarks == idx_bquark) & ~hasMatch, False)]
+            #print("Nbquark", akz.num(bquarks), end='\n\n')
+            #print("Njet", ak.num(self.events.JetGood), end='\n\n')
+            print("idx_match_candidates", idx_match_candidates, end='\n\n')
             #if idx_bquark == 0:
-            if idx_bquark == 0:
-                matchedJet     = ak.unflatten( ak.firsts(self.events.JetGood[idx_JetGood[( (idx_bquarks == idx_bquarks[:,idx_bquark]) & ~hasMatch )]]), 1 )
-                idx_matchedJet = ak.unflatten( ak.firsts(idx_JetGood[( (idx_bquarks == idx_bquarks[:,idx_bquark]) & ~hasMatch)]), 1 )
-                dr_matchedJet  = ak.unflatten( ak.firsts(deltaR[idx_pairs_sorted[( (idx_bquarks == idx_bquarks[:,idx_bquark]) & ~hasMatch)]]), 1 )
+            if idx_pair == 0:
+                #matchedJet     = ak.unflatten( ak.firsts(self.events.JetGood[idx_JetGood[( (idx_bquarks == idx_bquarks[:,idx_pair]) & ~hasMatch )]]), 1 )
+                idx_matchedJet    = ak.unflatten( ak.firsts(idx_match_candidates), 1 )
+                idx_matchedParton = ak.unflatten( idx_bquark, 1 )
+                dr_matchedJet     = ak.unflatten( ak.firsts(deltaR[idx_match_candidates]), 1 )
             else:
-                matchedJet     = ak.concatenate( (matchedJet, ak.unflatten( ak.firsts(self.events.JetGood[idx_JetGood[( (idx_bquarks == idx_bquarks[:,idx_bquark]) & ~hasMatch )]]), 1 ) ), axis=1 )
-                idx_matchedJet = ak.concatenate( (idx_matchedJet, ak.unflatten( ak.firsts(idx_JetGood[( (idx_bquarks == idx_bquarks[:,idx_bquark]) & ~hasMatch)]), 1 )), axis=1 )
-                dr_matchedJet  = ak.concatenate( (dr_matchedJet, ak.unflatten( ak.firsts(deltaR[idx_pairs_sorted[( (idx_bquarks == idx_bquarks[:,idx_bquark]) & ~hasMatch)]]), 1 ) ), axis=1 )
-            hasMatch = hasMatch | (idx_matchedJet == idx_matchedJet[:,idx_bquark])
+                if ak.all(ak.num(idx_matchedJet) == ak.num(bquarks)): break
+                #matchedJet     = ak.concatenate( (matchedJet, ak.unflatten( ak.firsts(self.events.JetGood[idx_JetGood[( (idx_bquarks == idx_bquarks[:,idx_pair]) & ~hasMatch )]]), 1 ) ), axis=1 )
+                idx_matchedJet    = ak.concatenate( (idx_matchedJet, ak.fill_none(ak.unflatten( ak.firsts(idx_match_candidates), 1 ), []) ), axis=1 )
+                idx_matchedParton = ak.concatenate( (idx_matchedParton, ak.unflatten( idx_bquark, 1 )), axis=1)
+                dr_matchedJet     = ak.concatenate( (dr_matchedJet, ak.unflatten( ak.firsts(deltaR[idx_match_candidates]), 1 ) ), axis=1 )
+            hasMatch = hasMatch | ak.fill_none(idx_JetGood == ak.fill_none(ak.firsts(idx_match_candidates), -99), False) | ak.fill_none(idx_bquarks == idx_bquark, False)
+            #print("idx_JetGood", idx_JetGood, end='\n\n')
+            #print("idx_match_candidates", idx_match_candidates, end='\n\n')
+            #hasMatch = hasMatch | (idx_JetGood == idx_match_candidates)
+        print("idx_matchedParton", idx_matchedParton)
+        print("idx_matchedJet", idx_matchedJet)
+        matchedJet = self.events.JetGood[idx_matchedJet]
         self.events["BQuark"] = ak.with_field(bquarks, matchedJet, "JetGoodMatched")
         self.events["BQuark"] = ak.with_field(self.events.BQuark, dr_matchedJet, "drMatchedJet")
         self.events["BQuark"] = ak.with_field(self.events.BQuark, ak.num(bquarks), "Nb")
