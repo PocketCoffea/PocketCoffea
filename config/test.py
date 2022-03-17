dccbd3cc8727e7df4ecf8c440c90e1f426857668
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2022-03-17 11:48:46 +0100

    New config file attributes

--- config/test.py
+++ config/test.py
@@ -1,6 +1,21 @@
 from lib.cuts import dilepton
 
 cfg =  {
+    # Input and output files
+    "workflow" : "base",
+    "input"    : "/work/mmarcheg/PocketCoffea/datasets/baseline_samples_local.json",
+    "output"   : "/work/mmarcheg/PocketCoffea/histograms/test.coffea",
+
+    # Executor parameters
+    "executor"     : "futures",
+    "workers"      : 12,
+    "scaleout"     : 6,
+    "chunk"        : 50000,
+    "max"          : None,
+    "limit"        : 1,
+    "skipbadfiles" : None,
+
+    # Cuts and variables to plot
     "cuts" : [dilepton],
     "variables" : [
         "muon_pt",
