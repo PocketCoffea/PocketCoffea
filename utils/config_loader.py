3f203ab972c3ba4abf944fae3bd5f093a6b62a69
mmarchegiani <matmarcheg@gmail.com>
2022-03-17 11:51:40 +0100

    Clean

--- utils/config_loader.py
+++
@@ -1,7 +0,0 @@
-import importlib.util
-
-def load_config(path):
-    spec = importlib.util.spec_from_file_location("cfg", path)
-    cfg = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(cfg)
-    return cfg
