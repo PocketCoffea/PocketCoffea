29941d0398ecccf9d62a60b684ef7286818d24c2
mmarchegiani <matmarcheg@gmail.com>
2022-03-18 22:40:54 +0100

    Prelimary version of plotting script

--- scripts/plot/make_plots.py
+++ scripts/plot/make_plots.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import json
 import argparse
@@ -17,47 +18,21 @@ from multiprocessing import Pool
 from parameters.allhistograms import histogram_settings
 from parameters.lumi import lumi
 
-from utils.config_loader import load_config
+from utils.Configurator import Configurator
 
 parser = argparse.ArgumentParser(description='Plot histograms from coffea file')
-parser.add_argument('-i', '--input', type=str, help='Input histogram filename', required=True)
-parser.add_argument('-o', '--output', type=str, default='', help='Output directory', required=True)
-parser.add_argument('--outputDir', type=str, default=None, help='Output directory')
 parser.add_argument('--cfg', default=os.getcwd() + "/config/test.json", help='Config file with parameters specific to the current run', required=False)
-parser.add_argument('-s', '--scale', type=str, default='linear', help='Plot y-axis scale', required=False)
-parser.add_argument('-d', '--dense', action='store_true', default=False, help='Normalized plots')
-parser.add_argument('--hist2d', action='store_true', default=False, help='Plot only 2D histograms')
-parser.add_argument('--proxy', action='store_true', help='Plot proxy and signal comparison')
-parser.add_argument('--only', action='store', default='', help='Plot only one histogram')
-parser.add_argument('--test', action='store_true', default=False, help='Test with lower stats.')
-parser.add_argument('--data', type=str, default='BTagMu', help='Data sample name')
-parser.add_argument('-n', '--normed', action='store_true', default=False, help='Normalized overlayed plots')
-parser.add_argument('-j', '--workers', type=int, default=12, help='Number of workers (cores/threads) to use for multi-worker execution (default: %(default)s)')
 
 args = parser.parse_args()
+config = Configurator(args.cfg, plot=True)
 
 print("Starting ", end='')
 print(time.ctime())
 start = time.time()
-print("Running with options:")
-print("    ", args)
 
-if os.path.isfile( args.input ): accumulator = load(args.input)
-else:
-    files_list = [ifile for ifile in os.listdir(args.input) if ifile != args.output]
-    accumulator = load(args.input + files_list[0])
-    histograms = accumulator.keys()
-    for ifile in files_list[1:]:
-        output = load(args.input + ifile)
-        for histname in histograms:
-            accumulator[histname].add(output[histname])
-
-cfg = load_config(args.cfg).cfg
-
-#scaleXS = {}
-#for isam in accumulator[next(iter(accumulator))].identifiers('dataset'):
-#   isam = str(isam)
-#   scaleXS[isam] = 1 if isam.startswith('BTag') else xsecs[isam]/accumulator['sumw'][isam]
+print( config.output )
+if os.path.isfile( config.output ): accumulator = load(config.output)
+else: sys.exit("Input file does not exist")
 
 data_err_opts = {
     'linestyle': 'none',
@@ -107,20 +82,20 @@ selection = {
 }
 
 plt.style.use([hep.style.ROOT, {'font.size': 16}])
-plot_dir = args.outputDir if args.outputDir else os.getcwd()+"/plots/" + args.output + "/"
+plot_dir = config.plots if config.plots else sys.exit("Plot directory is not defined")
 if not os.path.exists(plot_dir):
     os.makedirs(plot_dir)
 
 def make_plots(entrystart, entrystop):
     _accumulator = dict(list(accumulator.items())[entrystart:entrystop])
     for histname in _accumulator:
-        if args.only and not (args.only in histname): continue
-        if not histname.lstrip('hist_').startswith(tuple(cfg['variables'])): continue
+        if config.only and not (config.only in histname): continue
+        if not histname.lstrip('hist_').startswith(tuple(config.variables)): continue
         h = _accumulator[histname]
 
         for year in [str(s) for s in h.identifiers('year')]:
             # Convert lumi in fb^-1 and round to the first decimal digit
-            totalLumi = 'TEST' if args.test else round(lumi[year]/1000, 1)            
+            totalLumi = round(lumi[year]/1000, 1)            
             for cut in [str(s) for s in h.identifiers('cut')]:
                 selection_text = selection[cut]
                 datasets = [str(s) for s in h.identifiers('dataset')]
@@ -131,7 +106,7 @@ def make_plots(entrystart, entrystop):
                 #    h = h.rebin(varname, hist.Bin(varname, varlabel, **histogram_settings['variables']['_'.join(histname.split('_')[:2])]['binning']))
                 #h.scale( scaleXS, axis='dataset' )
 
-                if (not 'hist2d' in histname) & (not args.hist2d):
+                if not 'hist2d' in histname:
 
                     fig, ax = plt.subplots(1, 1, figsize=(12,9))
                     fig.subplots_adjust(hspace=.07)
@@ -159,13 +134,12 @@ def make_plots(entrystart, entrystop):
                             ax.set_xlim(0,200)
                         #ax.set_xlim(**histogram_settings['variables']['_'.join(histname.lstrip('hist_').split('_')[:2])]['xlim'])
                     filepath = f"{plot_dir}{histname}_{cut}_{year}.png"
-                    if args.scale != parser.get_default('scale'):
-                        if (not args.dense) & (args.scale == "log"):
-                            ax.semilogy()
-                            exp = 2 + math.floor(math.log(maxY, 10))
-                            ax.set_ylim(0.1, 10**exp)
+                    if config.scale == 'log':
+                        ax.semilogy()
+                        exp = 2 + math.floor(math.log(maxY, 10))
+                        ax.set_ylim(0.1, 10**exp)
                         #rax.set_ylim(0.1,10)
-                        filepath = filepath.replace(".png", "_" + args.scale + ".png")
+                        filepath = filepath.replace(".png", "_" + config.scale + ".png")
                     print("Saving", filepath)
                     plt.savefig(filepath, dpi=300, format="png")
                     plt.close(fig)
@@ -174,10 +148,10 @@ def make_plots(entrystart, entrystop):
 
 HistsToPlot = [k for k in accumulator.keys() if 'hist' in k]
 NtotHists = len(HistsToPlot)
-NHistsToPlot = len([key for key in HistsToPlot if args.only in key])
+NHistsToPlot = len([key for key in HistsToPlot if config.only in key])
 print("# tot histograms = ", NtotHists)
 print("# histograms to plot = ", NHistsToPlot)
-delimiters = np.linspace(0, NtotHists, args.workers + 1).astype(int)
+delimiters = np.linspace(0, NtotHists, config.workers + 1).astype(int)
 chunks = [(delimiters[i], delimiters[i+1]) for i in range(len(delimiters[:-1]))]
 pool = Pool()
 pool.starmap(make_plots, chunks)
