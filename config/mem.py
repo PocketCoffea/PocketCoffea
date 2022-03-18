c7434494c8f78330efaf4bcdc8fe86a90132ea29
mmarchegiani <matmarcheg@gmail.com>
2022-03-18 22:39:18 +0100

    Config files

--- config/mem.py
+++ config/mem.py
@@ -3,8 +3,9 @@ from lib.cuts import dilepton
 cfg =  {
     # Input and output files
     "workflow" : "mem"
-    "input"    : "/work/mmarcheg/PocketCoffea/datasets/baseline_samples_local.json",
-    "output"   : "/work/mmarcheg/PocketCoffea/histograms/mem_test.coffea",
+    "input"    : "datasets/baseline_samples_local.json",
+    "output"   : "histograms/mem_test.coffea",
+    "plots"    : "mem_test",
 
     # Executor parameters
     "executor"     : "futures",
@@ -18,32 +19,30 @@ cfg =  {
     # Cuts and variables to plot
     "cuts" : [dilepton],
     "variables" : [
-        "muon_pt",
-        "muon_eta",
-        "muon_phi",
-        "electron_pt",
-        "electron_eta",
-        "electron_phi",
-        "jet_pt",
-        "jet_eta",
-        "jet_phi",
-        "nmuon",
-        "nelectron",
-        "nlep",
-        "nmuongood",
-        "nelectrongood",
-        "nlepgood",
-        "njet",
-        "nbjet",
-        "nfatjet",
-        "nbquark",
-        "bquark_pt",
-        "bquark_eta",
-        "bquark_phi",
-        "bquark_drMatchedJet",
+        "muon_pt" : None,
+        "muon_eta" : None,
+        "muon_phi" : None,
+        "electron_pt" : None,
+        "electron_eta" : None,
+        "electron_phi" : None,
+        "jet_pt" : None,
+        "jet_eta" : None,
+        "jet_phi" : None,
+        "nmuon" : None,
+        "nelectron" : None,
+        "nlep" : None,
+        "nmuongood" : None,
+        "nelectrongood" : None,
+        "nlepgood" : None,
+        "njet" : None,
+        "nbjet" : None,
+        "nfatjet" : None,
+        "nbquark" : None,
+        "bquark_pt" : None,
+        "bquark_eta" : None,
+        "bquark_phi" : None,
+        "bquark_drMatchedJet" : None,
     ],
-    "variables2d" : [
-        
-    ],
+    "variables2d" : {},
     "scale_ttHbb" : 1000
 }
--- config/test.py
+++ config/test.py
@@ -3,8 +3,9 @@ from lib.cuts import dilepton
 cfg =  {
     # Input and output files
     "workflow" : "base",
-    "input"    : "/work/mmarcheg/PocketCoffea/datasets/baseline_samples_local.json",
-    "output"   : "/work/mmarcheg/PocketCoffea/histograms/test.coffea",
+    "input"    : "datasets/baseline_samples_local.json",
+    "output"   : "histograms/test.coffea",
+    "plots"    : "test",
 
     # Executor parameters
     "executor"     : "futures",
@@ -17,27 +18,25 @@ cfg =  {
 
     # Cuts and variables to plot
     "cuts" : [dilepton],
-    "variables" : [
-        "muon_pt",
-        "muon_eta",
-        "muon_phi",
-        "electron_pt",
-        "electron_eta",
-        "electron_phi",
-        "jet_pt",
-        "jet_eta",
-        "jet_phi",
-        "nmuon",
-        "nelectron",
-        "nlep",
-        "nmuongood",
-        "nelectrongood",
-        "nlepgood",
-        "njet",
-        "nbjet",
-        "nfatjet"
-    ],
-    "variables2d" : [
-        
-    ]
+    "variables" : {
+        "muon_pt" : None,
+        "muon_eta" : None,
+        "muon_phi" : None,
+        "electron_pt" : None,
+        "electron_eta" : None,
+        "electron_phi" : None,
+        "jet_pt" : None,
+        "jet_eta" : None,
+        "jet_phi" : None,
+        "nmuon" : None,
+        "nelectron" : None,
+        "nlep" : None,
+        "nmuongood" : None,
+        "nelectrongood" : None,
+        "nlepgood" : None,
+        "njet" : None,
+        "nbjet" : None,
+        "nfatjet" : None
+    },
+    "variables2d" : {}
 }
