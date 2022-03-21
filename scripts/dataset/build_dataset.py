7ea91deec9636feb010cebcfed94f4ce184f119a
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2022-03-21 23:03:56 +0100

    Script to build JSON dataset

--- scripts/dataset/build_dataset.py
+++ scripts/dataset/build_dataset.py
@@ -1,16 +1,16 @@
 import os
 import argparse
+
+from utils.Configurator import Configurator
 from utils.Dataset import Dataset
 
 parser = argparse.ArgumentParser(description='Build dataset file in json format')
-parser.add_argument('-i', '--input', default=os.getcwd() + "/dataset/DAS/RunIISummer20UL18.txt", help="JSON file containing the datasets' names on the DAS system", required=True)
+parser.add_argument('--cfg', default=os.getcwd() + "/config/test.py", help='Config file with parameters specific to the current run', required=False)
 
 args = parser.parse_args()
+config = Configurator(args.cfg)
 
-with open(args.input, 'r') as f:
-	datasets = f.readlines()
-
-for name in datasets:
-	dataset = Dataset(name)
-	print(dataset.filelist)
-	#dataset.Print()
+dataset       = Dataset(config.dataset, "root://xrootd-cms.infn.it//")
+dataset_local = Dataset(config.dataset, config.prefix)
+dataset.save(config.json)
+dataset_local.save(config.json.replace('.json', '_local.json'))
