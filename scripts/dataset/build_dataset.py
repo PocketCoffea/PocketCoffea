b27aaa031dcb86eadf499d438d1aec443708c09e
mmarchegiani <matmarcheg@gmail.com>
2022-03-21 23:20:49 +0100

    Updates to prepare download script

--- scripts/dataset/build_dataset.py
+++ scripts/dataset/build_dataset.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Build dataset file in json format'
 parser.add_argument('--cfg', default=os.getcwd() + "/config/test.py", help='Config file with parameters specific to the current run', required=False)
 
 args = parser.parse_args()
-config = Configurator(args.cfg)
+config = Configurator(args.cfg, dataset=True)
 
 dataset       = Dataset(config.dataset, "root://xrootd-cms.infn.it//")
 dataset_local = Dataset(config.dataset, config.prefix)
