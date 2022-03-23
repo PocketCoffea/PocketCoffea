82d8df70b2caada5ccc091cc925ca7f919d05703
mmarchegiani <matmarcheg@gmail.com>
2022-03-23 23:29:43 +0100

    Parameters of per-event selections

---
+++ parameters/selection.py
@@ -0,0 +1,14 @@
+# Per-event cuts applied to each event
+
+event_selection = {
+	"dilepton" : {
+		"METbranch" : {
+			'2016' : "MET",
+			'2017' : "METFixEE2017",
+			'2018' : "MET",
+		},
+		"pt_leading_lepton" : 25,
+		"met" : 40,
+		#"mll" :
+	}
+}
