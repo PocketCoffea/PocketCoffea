d1882f44d7419c965990ed3f9c80a7ac5a96e734
mmarchegiani <mmarcheg@t3ui02.psi.ch>
2022-03-31 17:50:28 +0200

    Add datasets

---
+++ config/testDownload.py
@@ -0,0 +1,59 @@
+from lib.cuts import dilepton
+
+cfg =  {
+    # Dataset parameters
+    "dataset"  : "datasets/DAS/RunIISummer20UL18.txt",
+    "json"     : "datasets/RunIISummer20UL18_testDownload.json",
+    "storage_prefix" : "/pnfs/psi.ch/cms/trivcat/store/user/mmarcheg/ttHbb",
+
+    # Input and output files
+    "workflow" : "base",
+    "input"    : "datasets/RunIISummer20UL18.json",
+    "output"   : "histograms/RunIISummer20UL18_limit2.coffea",
+    "plots"    : "plots/RunIISummer20UL18_limit2",
+
+    # Executor parameters
+    "run_options" : {
+        "executor"     : "futures",
+        "workers"      : 12,
+        "scaleout"     : 10,
+        "chunk"        : 50000,
+        "max"          : None,
+        "skipbadfiles" : None,
+        "voms"         : None,
+        "limit"        : 2,        
+    },
+
+    # Cuts and plots settings
+    "finalstate" : "dilepton",
+    "cuts_definition" : {
+        "baseline" : {
+            "f"   : dilepton,
+            "tag" : "dilepton"
+        },
+    },
+    "categories": {
+        "baseline" : {"baseline"},
+    },
+    "variables" : {
+        "muon_pt" : {'binning' : {'n_or_arr' : 200, 'lo' : 0, 'hi' : 2000}, 'xlim' : (0,500),  'xlabel' : "$p_{T}^{\mu}$ [GeV]"},
+        "muon_eta" : None,
+        "muon_phi" : None,
+        "electron_pt" : None,
+        "electron_eta" : None,
+        "electron_phi" : None,
+        "jet_pt" : None,
+        "jet_eta" : None,
+        "jet_phi" : None,
+        "nmuon" : None,
+        "nelectron" : None,
+        "nlep" : None,
+        "nmuon" : None,
+        "nelectron" : None,
+        "nlep" : None,
+        "njet" : None,
+        "nbjet" : None,
+    },
+    "variables2d" : {},
+    "scale" : "log"
+}
