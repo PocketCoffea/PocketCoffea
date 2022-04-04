4998bfad896053f354786b310d3d67ce378bd9d8
mmarchegiani <matmarcheg@gmail.com>
2022-04-04 22:15:58 +0200

    Introduce new library to fill histograms

---
+++ lib/fill.py
@@ -0,0 +1,22 @@
+from coffea import hist
+import awkward as ak
+
+def hist_is_in(histname, hist_list, accumulator):
+    isHist = type(accumulator[histname]) == hist.Hist
+    return ( isHist & (histname in hist_list) )
+
+def fill_histograms_object(processor, obj, obj_hists, event_var=False):
+    accumulator = processor.output
+    for histname in filter( lambda x : hist_is_in(x, obj_hists, accumulator), accumulator.keys() ):
+        #print(histname)
+        h = accumulator[histname]
+        for cut in processor._selections.keys():
+            if event_var:
+                weight = processor.weights.weight() * processor._cuts.all(*processor._selections[cut])
+                fields = {k: ak.fill_none(getattr(processor.events, k), -9999) for k in h.fields if k in histname}
+            else:
+                weight = ak.flatten( processor.weights.weight() * ak.Array(ak.ones_like(obj.pt) * processor._cuts.all(*processor._selections[cut])) )
+                fields = {k: ak.flatten(ak.fill_none(obj[k], -9999)) for k in h.fields if k in dir(obj)}
+            h.fill(dataset=processor._sample, cut=cut, year=processor._year, **fields, weight=weight)
+    #print("----------------------")
+
