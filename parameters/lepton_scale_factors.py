3a6b5997afdefd43c52547c998ac319d79e1974e
mmarchegiani <matmarcheg@gmail.com>
2022-05-19 12:18:46 +0200

    Implementation of electron id and reco SF

---
+++ parameters/lepton_scale_factors.py
@@ -0,0 +1,26 @@
+electronSF = {
+    'reco' : {
+        'pt>20' : "RecoAbove20",
+        'pt<20' : "RecoBelow20"
+    },
+    'id' : "wp80iso"
+}
+
+electronJSONfiles = {
+    '2016preVFP' : {
+        'file' : "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/EGM/2016preVFP_UL/electron.json.gz",
+        'name' : "UL-Electron-ID-SF"
+    },
+    '2016postVFP' : {
+        'file' : "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/EGM/2016postVFP_UL/electron.json.gz",
+        'name' : "UL-Electron-ID-SF"
+    },
+    '2017' : {
+        'file' : "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/EGM/2017_UL/electron.json.gz",
+        'name' : "UL-Electron-ID-SF"
+    },
+    '2018' : {
+        'file' : "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/EGM/2018_UL/electron.json.gz",
+        'name' : "UL-Electron-ID-SF"
+    }
+}
