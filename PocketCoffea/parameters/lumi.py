166b2b3e17ac70814b61ae074d62b7c471611047
mmarchegiani <matmarcheg@gmail.com>
2022-06-15 09:51:13 +0200

    Function to convert picobarn to femtobarn

--- PocketCoffea/parameters/lumi.py
+++ PocketCoffea/parameters/lumi.py
@@ -1,3 +1,10 @@
+# Function to convert and round the integrated luminosity from picobarn to femtobarn
+def femtobarn(picobarn, digits=None):
+    if round:
+        return round((picobarn)/1000., digits)
+    else:
+        return picobarn/1000.
+
 # Integrated luminosity [pb^-1] for each data-taking year
 
 lumi = {
