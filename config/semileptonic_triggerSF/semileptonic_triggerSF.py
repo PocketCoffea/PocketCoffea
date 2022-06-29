b42f14b2d179beb7df3056940114a289b9eae336
mmarchegiani <matmarcheg@gmail.com>
2022-06-29 12:01:44 +0200

    Config files for trigger eff

--- config/semileptonic_triggerSF/semileptonic_triggerSF.py
+++ config/semileptonic_triggerSF/semileptonic_triggerSF.py
@@ -1,6 +1,7 @@
 from PocketCoffea.parameters.cuts.baseline_cuts import semileptonic_triggerSF_presel, passthrough
 from config.semileptonic_triggerSF.functions import get_trigger_passfail
 from PocketCoffea.workflows.semileptonic_triggerSF import semileptonicTriggerProcessor
+from math import pi
 
 cfg =  {
 
@@ -15,11 +16,11 @@ cfg =  {
 
     # Input and output files
     "workflow" : semileptonicTriggerProcessor,
-    "output"   : "output/semileptonic_triggerSF",
+    "output"   : "output/semileptonic_triggerSF_2018",
 
     # Executor parameters
     "run_options" : {
-        "executor"       : "futures",
+        "executor"       : "parsl/slurm",
         "workers"        : 12,
         "scaleout"       : 10,
         "partition"      : "standard",
@@ -30,20 +31,20 @@ cfg =  {
         "max"            : None,
         "skipbadfiles"   : None,
         "voms"           : None,
-        "limit"          : 2,
+        "limit"          : None,
     },
 
     # Cuts and plots settings
     "finalstate" : "semileptonic_triggerSF",
     "preselections" : [semileptonic_triggerSF_presel],
     "categories": {
-        "Ele32_WPTight_Gsf_pass" : [get_trigger_passfail("Ele32_WPTight_Gsf", "pass")],
-        "Ele32_WPTight_Gsf_fail" : [get_trigger_passfail("Ele32_WPTight_Gsf", "fail")],
+        "Ele32_EleHT_pass" : [get_trigger_passfail(["Ele32_WPTight_Gsf", "Ele28_eta2p1_WPTight_Gsf_HT150"], "pass")],
+        "Ele32_EleHT_fail" : [get_trigger_passfail(["Ele32_WPTight_Gsf", "Ele28_eta2p1_WPTight_Gsf_HT150"], "fail")],
         "inclusive" : [passthrough],
     },
 
     # List of triggers for SF measurement
-    "triggers_to_measure" : ["Ele32_WPTight_Gsf"],
+    "triggers_to_measure" : ["Ele32_WPTight_Gsf", "Ele28_eta2p1_WPTight_Gsf_HT150"],
     
     "variables" : {
         "muon_pt" : {'binning' : {'n_or_arr' : 200, 'lo' : 0, 'hi' : 2000}, 'xlim' : (0,500),  'xlabel' : "$p_{T}^{\mu}$ [GeV]"},
@@ -61,6 +62,24 @@ cfg =  {
         "njet" : None,
         "nbjet" : None,
     },
-    "variables2d" : {},
-    "scale" : "log"
+    "variables2d" : {
+        'electron_etaSC_vs_electron_pt' : {
+            'electron_pt'               : {'binning' : {'n_or_arr' : [0, 25, 35, 45, 55, 100, 500, 2000]},  'xlim' : (20,500),    'xlabel' : "Electron $p_{T}$ [GeV]",
+                                           'xticks' : [20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500]},
+            'electron_etaSC'            : {'binning' : {'n_or_arr' : [-2.5, -1.5660, -1.4442, -0.8, 0.0, 0.8, 1.4442, 1.5660, 2.5]}, 'ylim' : (-2.5,2.5), 'ylabel' : "Electron Supercluster $\eta$"},
+        },
+        'electron_phi_vs_electron_pt' : {
+            'electron_pt'               : {'binning' : {'n_or_arr' : [0, 25, 35, 45, 55, 100, 500, 2000]},  'xlim' : (20,500),    'xlabel' : "Electron $p_{T}$ [GeV]",
+                                           'xticks' : [20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500]},
+            'electron_phi'              : {'binning' : {'n_or_arr' : 12, 'lo' : -pi, 'hi' : pi}, 'ylim' : (-pi,pi), 'ylabel' : "$\phi_{e}$"},
+        },
+        'electron_etaSC_vs_electron_phi' : {
+            'electron_phi'              : {'binning' : {'n_or_arr' : 12, 'lo' : -pi, 'hi' : pi}, 'xlim' : (-pi,pi),    'xlabel' : "$\phi_{e}$"},
+            'electron_etaSC'            : {'binning' : {'n_or_arr' : [-2.5, -1.5660, -1.4442, -0.8, 0.0, 0.8, 1.4442, 1.5660, 2.5]}, 'ylim' : (-2.5,2.5), 'ylabel' : "Electron Supercluster $\eta$"},
+        },
+    },
+    "plot_options" : {
+        "scale" : "log",
+        "fontsize" : 14,
+    }
 }
