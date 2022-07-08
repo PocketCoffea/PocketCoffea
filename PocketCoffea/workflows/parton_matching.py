a71778d4e8bf1105d7e708a0db17475b134cddf2
Davide Valsecchi <davide.valsecchi@cern.ch>
2022-07-08 15:58:57 +0200

    Added some predefined cut function getters

--- PocketCoffea/workflows/parton_matching.py
+++ PocketCoffea/workflows/parton_matching.py
@@ -15,9 +15,9 @@ class PartonMatchingProcessor(ttHbbBaseProcessor):
         more_histos = {}
         more_histos["hist_ptComparison_parton_matching"] = hist.Hist("hist_parton_matching", self._sample_axis, self._cat_axis,
                                         self._year_axis,
-                                        hist.Bin("jet_pt", "Jet pT", 50, 0, 400),
-                                        hist.Bin("parton_pt","Parton pt", 50, 0, 400),
-                                        hist.Bin("jet_eta","Jet Eta", 10, -5, 5),
+                                        hist.Bin("jet_pt", "Jet pT", 40, 0, 300),
+                                        hist.Bin("parton_pt","Parton pt", 40, 0, 300),
+                                        hist.Bin("jet_eta","Jet Eta", 5, -2.4, 2.4),
                                         )
         more_histos["hist_deltaR_parton_matching"] = hist.Hist("hist_deltaR_parton_jet", self._sample_axis, self._cat_axis,
                                       self._year_axis,
@@ -35,10 +35,8 @@ class PartonMatchingProcessor(ttHbbBaseProcessor):
         
         # Select b-quarks at Gen level, coming from H->bb decay
         if self.events.metadata["sample"] == 'ttHTobb':
-            isHiggs = self.events.GenPart.pdgId == 25
-            isHard = self.events.GenPart.hasFlags(['fromHardProcess'])
-            hasTwoChildren = ak.num(self.events.GenPart.childrenIdxG, axis=2) == 2
-            higgs = self.events.GenPart[isHiggs & isHard & hasTwoChildren]
+            higgs = self.events.GenPart[(self.events.GenPart.pdgId == 25) & (self.events.GenPart.hasFlags(['fromHardProcess']))]
+            higgs =  higgs[ak.num(higgs.childrenIdxG, axis=2) == 2]
             quarks = ak.concatenate( (quarks, ak.flatten(higgs.children, axis=2)), axis=1 )
             # Sort b-quarks by pt
             quarks = ak.with_name(quarks[ak.argsort(quarks.pt, ascending=False)], name='PtEtaPhiMCandidate')
@@ -60,11 +58,17 @@ class PartonMatchingProcessor(ttHbbBaseProcessor):
     def fill_histograms_extra(self):
         fill_histograms_object(self, self.events.Parton, self.parton_hists)
         # Fill the parton matching 2D plot
+        hjetpartontotal = self.get_histogram("hist2d_Njet_Nparton_total")
         hmatched = self.get_histogram("hist2d_Njet_Nparton_matched")
+        hppmatched = self.get_histogram("hist2d_Nparton_Nparton_matched")
         for category, cuts in self._categories.items():
             weight = self._cuts_masks.all(*cuts)
             hmatched.fill(sample=self._sample, cat=category, year=self._year, 
                            Njet=self.events.njet, Nparton=self.events.nparton_matched, weight=weight)
+            hjetpartontotal.fill(sample=self._sample, cat=category, year=self._year, 
+                           Njet=self.events.njet, Nparton=self.events.nparton, weight=weight)
+            hppmatched.fill(sample=self._sample, cat=category, year=self._year, 
+                            Nparton=self.events.nparton,  Nparton_matched=self.events.nparton_matched, weight=weight)
 
         hpts = self.get_histogram("hist_ptComparison_parton_matching")
         hdeltaR = self.get_histogram("hist_deltaR_parton_matching")
@@ -76,7 +80,7 @@ class PartonMatchingProcessor(ttHbbBaseProcessor):
         for category, cuts in self._categories.items():
             weight = ak.flatten( (ak.ones_like(self.events.JetGoodMatched.pt)[self.matched_partons_mask]) * self._cuts_masks.all(*cuts))
             hpts.fill(sample=self._sample, cat=category, year=self._year, 
-                      jet_pt=jet_pt, parton_pt=parton_pt, jet_eta=jet_eta, weight=weight)
+                     jet_pt=jet_pt, parton_pt=parton_pt, jet_eta=jet_eta, weight=weight)
             hdeltaR.fill(sample=self._sample, cat=category, year=self._year, 
                       deltaR=delta_R, weight=weight)
         
--- config/parton_matching/config.py
+++ config/parton_matching/config.py
@@ -1,5 +1,5 @@
 from PocketCoffea.parameters.cuts.baseline_cuts import dilepton_presel, semileptonic_presel, passthrough
-from PocketCoffea.lib.cut_functions import count_objects_gt, get_nJets_min
+from PocketCoffea.lib.cut_functions import get_nObj, get_nBtag
 from PocketCoffea.lib.cut_definition import Cut
 from config.parton_matching.functions import *
 from PocketCoffea.workflows.parton_matching  import PartonMatchingProcessor
@@ -9,7 +9,7 @@ cfg =  {
         "jsons": ["datasets/signal_ttHTobb_2018_local.json",
                   "datasets/backgrounds_MC_2018_local.json"],
         "filter" : {
-            "samples": ["TTToSemiLeptonic", "ttHTobb"],
+            "samples": ["TTToSemiLeptonic"],
             "samples_exclude" : [],
             "year": ["2018"]
         }
@@ -19,33 +19,55 @@ cfg =  {
     "workflow" : PartonMatchingProcessor,
     "output"   : "output/parton_matching",
 
+
+    # "run_options" : {
+    #     "executor"       : "iterative",
+    #     "workers"        : 1,
+    #     "scaleout"       : 60,
+    #     "partition"      : "standard",
+    #     "walltime"       : "03:00:00",
+    #     "mem_per_worker" : "4GB", # GB
+    #     "exclusive"      : False,
+    #     "chunk"          : 500000,
+    #     "retries"        : 30,
+    #     "max"            : None,
+    #     "skipbadfiles"   : None,
+    #     "voms"           : None,
+    #     "limit"          : 10,
+    # },
     "run_options" : {
         "executor"       : "dask/slurm",
         "workers"        : 1,
-        "scaleout"       : 30,
+        "scaleout"       : 50,
         "partition"      : "standard",
-        "walltime"       : "03:00:00",
-        "mem_per_worker" : "3GB", # GB
+        "walltime"       : "02:00:00",
+        "mem_per_worker" : "5GB", # GB
         "exclusive"      : False,
         "chunk"          : 100000,
         "retries"        : 30,
+        "treereduction"  : 10,
         "max"            : None,
         "skipbadfiles"   : None,
         "voms"           : None,
-        "limit"          : 100,
+        "limit"          : None,
     },
-    
+
+
     # Cuts and plots settings
     "finalstate" : "semileptonic",
+    "skim": [ get_nBtag(3, 20., "Jet") ],
     "preselections" : [semileptonic_presel],
 
     "categories": {
-        "4j_3bjets" : [passthrough],
-        "4j_4bjets" : [getNjetNb_cut(4,4)],
+        "4j" : [passthrough],
+        "5j" : [ get_nObj(5, coll="JetGood")],
+        "6j" : [ get_nObj(6, coll="JetGood")],
+        "7j" : [ get_nObj(7, coll="JetGood")],
+        "8j" : [ get_nObj(8, coll="JetGood")],
     },
     
     "variables" : {
-        "muon_pt" : {'binning' : {'n_or_arr' : 200, 'lo' : 0, 'hi' : 2000}, 'xlim' : (0,500),  'xlabel' : "$p_{T}^{\mu}$ [GeV]"},
+        "muon_pt" : {'binning' : {'n_or_arr' : 100, 'lo' : 0, 'hi' : 500}, 'xlim' : (0,500),  'xlabel' : "$p_{T}^{\mu}$ [GeV]"},
         "muon_eta" : None,
         "muon_phi" : None,
         "electron_pt" : None,
@@ -71,11 +93,18 @@ cfg =  {
 
     },
      "variables2d" : {
-
+        "Njet_Nparton_total": {
+            "Njet": { 'binning': {"n_or_arr": 11, 'lo': 4, 'hi':15}, "xlabel": "N jets"},
+            "Nparton" : { 'binning': {"n_or_arr": 13, 'lo': 2, 'hi':15}, "ylabel": "N partons"}
+        },
         "Njet_Nparton_matched": {
-            "Njet": { 'binning': {"n_or_arr": 15, 'lo': 0, 'hi':15}, "xlabel": "N jets"},
-            "Nparton" : { 'binning': {"n_or_arr": 15, 'lo': 0, 'hi':15}, "ylabel": "N partons"}
-     }
+            "Njet": { 'binning': {"n_or_arr": 6, 'lo': 2, 'hi':8}, "xlabel": "N jets"},
+            "Nparton" : { 'binning': {"n_or_arr": 6, 'lo': 2, 'hi':8}, "ylabel": "N partons"}
+        },
+        "Nparton_Nparton_matched": {
+            "Nparton": { 'binning': {"n_or_arr": 6, 'lo': 2, 'hi':8}, "xlabel": "N partons"},
+            "Nparton_matched" : { 'binning': {"n_or_arr": 6, 'lo': 2, 'hi':8}, "ylabel": "N partons matched"}
+        }
      },
     "scale" : "log"
 }
