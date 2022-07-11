ccdee45c9fb04918ef7842d4e21bc9f4235770e2
mmarchegiani <matmarcheg@gmail.com>
2022-07-11 08:14:35 +0200

    wip: systematics variations in trigger SF

--- PocketCoffea/workflows/semileptonic_triggerSF.py
+++ PocketCoffea/workflows/semileptonic_triggerSF.py
@@ -7,18 +7,29 @@ from coffea.analysis_tools import Weights
 from ..workflows.base import ttHbbBaseProcessor
 from ..lib.pileup import sf_pileup_reweight
 from ..lib.scale_factors import sf_ele_reco, sf_ele_id, sf_mu
+from ..lib.fill import fill_histograms_object_with_variations
 from ..parameters.lumi import lumi
 from ..parameters.samples import samples_info
 
 class semileptonicTriggerProcessor(ttHbbBaseProcessor):
     def __init__(self,cfg) -> None:
         super().__init__(cfg=cfg)
+        pileup_axis   = hist.Cat("pileup", "Pileup")
+        sf_ele_reco_axis = hist.Cat("sf_ele_reco", "SF (ele, reco)")
+        sf_ele_id_axis   = hist.Cat("sf_ele_id", "SF (ele, ID)")
+        for histname, h in self._hist_dict.items():
+            self._hist_dict[histname] = hist.Hist("$N_{events}$", *h.axes(), pileup_axis, sf_ele_reco_axis, sf_ele_id_axis)
+        for histname, h in self._hist2d_dict.items():
+            self._hist2d_dict[histname] = hist.Hist("$N_{events}$", *h.axes(), pileup_axis, sf_ele_reco_axis, sf_ele_id_axis)
         # Accumulator with sum of weights of the events passing each category for each sample
         self._eff_dict = {cat: defaultdict_accumulator(float) for cat in self._categories}
         self._accum_dict["trigger_efficiency"] = dict_accumulator(self._eff_dict)
         # Add 2D histogram for trigger efficiency computation
         self.efficiency_maps = ["hist2d_electron_etaSC_vs_electron_pt", "hist2d_electron_phi_vs_electron_pt", "hist2d_electron_etaSC_vs_electron_phi"]
         self.electron_hists = self.electron_hists + self.efficiency_maps
+        self._accum_dict.update(self._hist_dict)
+        self._accum_dict.update(self._hist2d_dict)
+        self._accumulator = processor.dict_accumulator(self._accum_dict)
 
     # Overwrite the method `compute_weights()` in order to take into account the weights of all the corrections previously applied
     # In this way, even if the method is modified in the base processor, only the weights reported here are used for the event reweighting.
@@ -36,6 +47,12 @@ class semileptonicTriggerProcessor(ttHbbBaseProcessor):
             # Muon id and iso SF with nominal, up and down variations
             self.weights.add('sf_mu_id',  *sf_mu(self.events, self._year, 'id'))
             self.weights.add('sf_mu_iso', *sf_mu(self.events, self._year, 'iso'))
+
+    def fill_histograms(self):
+        variations = ['pileup', 'sf_ele_reco', 'sf_ele_id']
+        for (obj, obj_hists) in zip([self.events.MuonGood, self.events.ElectronGood, self.events.JetGood], [self.muon_hists, self.electron_hists, self.jet_hists]):
+        #for (obj, obj_hists) in zip([self.events.ElectronGood], [self.electron_hists]):
+            fill_histograms_object_with_variations(self, obj, obj_hists, variations)
 
     def postprocess(self, accumulator):
         super().postprocess(accumulator=accumulator)
