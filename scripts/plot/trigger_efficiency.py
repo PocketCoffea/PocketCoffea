78b02d8a9ddec35515c61879016334c768c44a5f
mmarchegiani <matmarcheg@gmail.com>
2022-07-13 18:09:55 +0200

    Clean plotting script

--- scripts/plot/trigger_efficiency.py
+++ scripts/plot/trigger_efficiency.py
@@ -47,35 +47,20 @@ if not os.path.exists(plot_dir):
     os.makedirs(plot_dir)
 
 print(accumulator.keys())
-#accumulator1d = dict( [(key, value) for key, value in accumulator.items() if key.startswith('hist_electron_')] )
-#accumulator2d = dict( [(key, value) for key, value in accumulator.items() if key.startswith('hist2d_')] )
 
 def _plot_efficiency(entrystart, entrystop):
     _accumulator = dict( [(key, value) for key, value in accumulator.items() if key.startswith('hist')][entrystart:entrystop] )
-    print(_accumulator)
     plot_efficiency(_accumulator, config)
 
-def _plot_efficiency_2d(entrystart, entrystop):
-    _accumulator2d = dict( [(key, value) for key, value in accumulator2d.items()][entrystart:entrystop] )
-    plot_efficiency_2d(_accumulator2d, config)
-
 HistsToPlot   = [k for k in accumulator.keys() if k.startswith('hist')]
-#Hists2dToPlot = [k for k in accumulator.keys() if k.startswith('hist2d_')]
 NtotHists   = len(HistsToPlot)
-#NtotHists2d = len(Hists2dToPlot)
 NHistsToPlot   = len([key for key in HistsToPlot if config.only in key])
-#NHists2dToPlot = len([key for key in Hists2dToPlot if config.only in key])
 print("# tot histograms = ", NtotHists)
 print("# histograms to plot = ", NHistsToPlot)
 print("histograms to plot:", HistsToPlot)
-#print("# tot 2D-histograms = ", NtotHists2d)
-#print("# 2D-histograms to plot = ", NHists2dToPlot)
-#print("2D-histograms to plot:", Hists2dToPlot)
-#delimiters_1d = np.linspace(0, NtotHists, config.plot_options['workers'] + 1).astype(int)
-#chunks_1d     = [(delimiters_1d[i], delimiters_1d[i+1]) for i in range(len(delimiters_1d[:-1]))]
 delimiters = np.linspace(0, NtotHists, config.plot_options['workers'] + 1).astype(int)
 chunks     = [(delimiters[i], delimiters[i+1]) for i in range(len(delimiters[:-1]))]
-print("chunks:", chunks)
+#print("chunks:", chunks)
 pool = Pool()
 pool.starmap(_plot_efficiency, chunks)
 pool.close()
