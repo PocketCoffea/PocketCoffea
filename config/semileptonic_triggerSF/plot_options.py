06209e9109b76b4d505da81f438b098d3c6d3187
mmarchegiani <matmarcheg@gmail.com>
2022-08-08 17:08:13 +0200

    Update config

--- config/semileptonic_triggerSF/plot_options.py
+++ config/semileptonic_triggerSF/plot_options.py
@@ -19,3 +19,25 @@ scalefactor = {
         }
     },
 }
+
+ratio = {
+    '2018' : {
+        'Ele32_EleHT_pass': {
+            'hist_electron_pt' : {'ylim' : (0.95, 1.05)},
+            'hist_electron_eta' : {'ylim' : (0.95, 1.05)},
+            'hist_electron_etaSC' : {'ylim' : (0.95, 1.05)},
+            'hist_electron_phi' : {'ylim' : (0.95, 1.05)},
+        }
+    },
+}
+
+residue = {
+    '2018' : {
+        'Ele32_EleHT_pass': {
+            'hist_electron_pt' : {'ylim' : (-0.05, 0.05)},
+            'hist_electron_eta' : {'ylim' : (-0.05, 0.05)},
+            'hist_electron_etaSC' : {'ylim' : (-0.05, 0.05)},
+            'hist_electron_phi' : {'ylim' : (-0.05, 0.05)},
+        }
+    },
+}
