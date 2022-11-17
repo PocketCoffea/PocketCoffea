cec9523f8d8b0f5dd29391957c87627baf8b68b2
mmarchegiani <matmarcheg@gmail.com>
2022-11-17 11:06:15 +0100

    Plotting upgrades and fix ttHbb

--- scripts/plot/make_plots.py
+++ scripts/plot/make_plots.py
@@ -29,6 +29,8 @@ parser = argparse.ArgumentParser(description='Plot histograms from coffea file')
 parser.add_argument('--cfg', default=os.getcwd() + "/config/test.json", help='Config file with parameters specific to the current run', required=False)
 parser.add_argument('-v', '--version', type=str, default=None, help='Version of output (e.g. `v01`, `v02`, etc.)')
 parser.add_argument('--test', default=False, action='store_true', help='Test mode')
+parser.add_argument('-j', '--workers', type=int, default=8, help='Number of parallel workers to use for plotting')
+parser.add_argument('-o', '--only', type=str, default='', help='Filter histograms name with string')
 
 args = parser.parse_args()
 config = Configurator(args.cfg, plot=True, plot_version=args.version)
@@ -113,13 +115,17 @@ def make_plots(entrystart, entrystop):
     for (histname, h) in _accumulator['variables'].items():
         plot_data_mc_hist1D(h, histname, config)
 
-HistsToPlot   = [k for k in accumulator['variables'].keys()]
+# Filter dictionary of histograms with `args.only`
+accumulator['variables'] = { k : v for k,v in accumulator['variables'].items() if args.only in k }
+HistsToPlot = [k for k in accumulator['variables'].keys()]
 NtotHists = len(HistsToPlot)
-NHistsToPlot = len([key for key in HistsToPlot if config.only in key])
+NHistsToPlot = len([key for key in HistsToPlot if args.only in key])
 print("# tot histograms = ", NtotHists)
 print("# histograms to plot = ", NHistsToPlot)
 print("Histograms to plot:", HistsToPlot)
-delimiters = np.linspace(0, NtotHists, config.run_options['workers'] + 1).astype(int)
+
+# Parallelization of plotting
+delimiters = np.linspace(0, NtotHists, args.workers + 1).astype(int)
 chunks = [(delimiters[i], delimiters[i+1]) for i in range(len(delimiters[:-1]))]
 pool = Pool()
 pool.starmap(make_plots, chunks)
