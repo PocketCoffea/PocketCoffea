ff0723fcaa6a0428cfb54ea3dea9f6fc3e3d1d62
mmarchegiani <matmarcheg@gmail.com>
2022-12-07 14:19:48 +0100

    pt reweighting parameters

--- pocket_coffea/parameters/custom/pt_reweighting/pt_reweighting.py
+++ pocket_coffea/parameters/custom/pt_reweighting/pt_reweighting.py
@@ -3,8 +3,8 @@ import pocket_coffea
 basefolder = pocket_coffea.__path__[0]
 
 pt_corrections = {
-	'2018' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2018UL_RunIISummer20UL/pt_corr_2018.coffea",
-	#'2017' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2017UL/pt_corr_2017.coffea",
-	'2016_PostVFP' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2016UL_PostVFP_v03/pt_corr_2016_PostVFP.coffea",
-	'2016_PreVFP' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2016UL_PreVFP_v04/pt_corr_2016_PreVFP.coffea",
+	'2018' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2018UL/pt_corr_1D_2018.json",
+	'2017' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2017UL/pt_corr_1D_2017.json",
+	'2016_PostVFP' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2016UL_PostVFP/pt_corr_1D_2016_PostVFP.json",
+	#'2016_PreVFP' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2016UL_PreVFP_v04/pt_corr_2016_PreVFP.coffea",
 }
