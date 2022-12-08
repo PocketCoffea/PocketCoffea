ad421cfe1cd05df13dde2e94f799c97fd1c0ec52
mmarchegiani <matmarcheg@gmail.com>
2022-12-08 18:25:58 +0100

    Add 2016_PreVFP and 2017 pt reweighting

--- pocket_coffea/parameters/custom/pt_reweighting/pt_reweighting.py
+++ pocket_coffea/parameters/custom/pt_reweighting/pt_reweighting.py
@@ -6,5 +6,5 @@ pt_corrections = {
 	'2018' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2018UL/pt_corr_1D_2018.json",
 	'2017' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2017UL/pt_corr_1D_2017.json",
 	'2016_PostVFP' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2016UL_PostVFP/pt_corr_1D_2016_PostVFP.json",
-	#'2016_PreVFP' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2016UL_PreVFP_v04/pt_corr_2016_PreVFP.coffea",
+	'2016_PreVFP' : f"{basefolder}/parameters/custom/pt_reweighting/pt_reweighting_2016UL_PreVFP/pt_corr_1D_2016_PreVFP.json",
 }
--- pocket_coffea/parameters/dask_env.py
+++ pocket_coffea/parameters/dask_env.py
@@ -1,7 +1,7 @@
 
 def setup_dask(dask_config):
     dask_config.set({"distributed.scheduler.allowed-failures": 10})
-    dask_config.set({"distributed.scheduler.work-stealing": False})
+    dask_config.set({"distributed.scheduler.work-stealing": True})
     dask_config.set({"distributed.scheduler.default-task-durations.processor": "3s"})
     dask_config.set({"distributed.scheduler.default-task-durations.reduce": "1s"})
     dask_config.set({"distributed.worker.memory.target": 0.7})
