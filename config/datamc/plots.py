e88f55b07223c5aab7c5f623641be0ce060cb5f3
mmarchegiani <matmarcheg@gmail.com>
2023-03-09 02:02:04 +0100

    2017UL config files

--- config/datamc/plots.py
+++ config/datamc/plots.py
@@ -4,11 +4,8 @@ cfg_plot = {
         "ttHTobb": "$t\\bar{t}H\\rightarrow b\\bar{b}$",
         "TTToSemiLeptonic": "$t\\bar{t}$ semilep.",
         "TTTo2L2Nu" : "$t\\bar{t}$ dilepton",
-        "ST_s-channel_4f_leptonDecays" : "ST_s-channel",
-        "ST_t-channel_top_4f_InclusiveDecays" : "ST_t-channel $t$",
-        "ST_t-channel_antitop_4f_InclusiveDecays" : "ST_t-channel $\\bar{t}$",
-        "ST_tW_top_5f_NoFullyHadronicDecays" : "ST_tW $t$",
-        "ST_tW_antitop_5f_NoFullyHadronicDecays" : "ST_tW $\\bar{t}$"
+        "SingleTop" : "Single t",
+        "WJetsToLNu_HT" : "W+jets",
     },
     "variables": {
         "JetGood_pt_1": {'xlim' : (0,500)},
--- config/datamc/ttHbb_ttbar_ST_WJets_datamc_2017.py
+++ config/datamc/ttHbb_ttbar_ST_WJets_datamc_2017.py
@@ -1,6 +1,6 @@
 from pocket_coffea.parameters.cuts.preselection_cuts import *
 from pocket_coffea.workflows.tthbb_base_processor import ttHbbBaseProcessor
-from pocket_coffea.lib.cut_functions import get_nObj_min, get_nObj_eq, get_HLTsel, get_nBtag, get_nElectron, get_nMuon
+from pocket_coffea.lib.cut_functions import get_nObj_min, get_nObj_eq, get_HLTsel, get_nBtagMin, get_nElectron, get_nMuon
 from pocket_coffea.parameters.histograms import *
 from pocket_coffea.parameters.btag import btag_variations
 from pocket_coffea.parameters.lepton_scale_factors import sf_ele_trigger_variations
@@ -9,11 +9,8 @@ from config.datamc.plots import cfg_plot
 samples = ["ttHTobb",
            "TTToSemiLeptonic",
            "TTTo2L2Nu",
-           "ST_s-channel_4f_leptonDecays",
-           "ST_t-channel_top_4f_InclusiveDecays",
-           "ST_t-channel_antitop_4f_InclusiveDecays",
-           "ST_tW_top_5f_NoFullyHadronicDecays",
-           "ST_tW_antitop_5f_NoFullyHadronicDecays",
+           "ST",
+           "WJetsToLNu_HT",
            "DATA_SingleEle",
            "DATA_SingleMuon"]
 
@@ -32,23 +29,23 @@ cfg =  {
         "filter" : {
             "samples": samples,
             "samples_exclude" : [],
-            "year": ['2018']
+            "year": ['2017']
         },
         "subsamples": subsamples
     },
 
     # Input and output files
     "workflow" : ttHbbBaseProcessor,
-    "output"   : "output/datamc/ttHbb_ttbar_ST_datamc_btagcalibrated_SingleMuon_bugfix",
+    "output"   : "output/datamc/ttHbb_ttbar_ST_WJets_datamc_2017",
     "worflow_options" : {},
 
     "run_options" : {
         "executor"       : "dask/slurm",
         "workers"        : 1,
-        "scaleout"       : 150,
+        "scaleout"       : 300,
         "queue"          : "standard",
         "walltime"       : "12:00:00",
-        "mem_per_worker" : "4GB", # GB
+        "mem_per_worker" : "6GB", # GB
         "exclusive"      : False,
         "chunk"          : 400000,
         "retries"        : 50,
@@ -67,22 +64,22 @@ cfg =  {
     "preselections" : [semileptonic_presel_nobtag],
     "categories": {
         "baseline": [passthrough],
-        "SingleEle_1b" : [ get_nElectron(1, coll="ElectronGood"), get_nBtag(1, coll="BJetGood") ],
-        "SingleEle_2b" : [ get_nElectron(1, coll="ElectronGood"), get_nBtag(2, coll="BJetGood") ],
-        "SingleEle_3b" : [ get_nElectron(1, coll="ElectronGood"), get_nBtag(3, coll="BJetGood") ],
-        "SingleEle_4b" : [ get_nElectron(1, coll="ElectronGood"), get_nBtag(4, coll="BJetGood") ],
-        "SingleMuon_1b" : [ get_nMuon(1, coll="MuonGood"), get_nBtag(1, coll="BJetGood") ],
-        "SingleMuon_2b" : [ get_nMuon(1, coll="MuonGood"), get_nBtag(2, coll="BJetGood") ],
-        "SingleMuon_3b" : [ get_nMuon(1, coll="MuonGood"), get_nBtag(3, coll="BJetGood") ],
-        "SingleMuon_4b" : [ get_nMuon(1, coll="MuonGood"), get_nBtag(4, coll="BJetGood") ],
-        "SingleEle_1b_btagcalibrated" : [ get_nElectron(1, coll="ElectronGood"), get_nBtag(1, coll="BJetGood") ],
-        "SingleEle_2b_btagcalibrated" : [ get_nElectron(1, coll="ElectronGood"), get_nBtag(2, coll="BJetGood") ],
-        "SingleEle_3b_btagcalibrated" : [ get_nElectron(1, coll="ElectronGood"), get_nBtag(3, coll="BJetGood") ],
-        "SingleEle_4b_btagcalibrated" : [ get_nElectron(1, coll="ElectronGood"), get_nBtag(4, coll="BJetGood") ],
-        "SingleMuon_1b_btagcalibrated" : [ get_nMuon(1, coll="MuonGood"), get_nBtag(1, coll="BJetGood") ],
-        "SingleMuon_2b_btagcalibrated" : [ get_nMuon(1, coll="MuonGood"), get_nBtag(2, coll="BJetGood") ],
-        "SingleMuon_3b_btagcalibrated" : [ get_nMuon(1, coll="MuonGood"), get_nBtag(3, coll="BJetGood") ],
-        "SingleMuon_4b_btagcalibrated" : [ get_nMuon(1, coll="MuonGood"), get_nBtag(4, coll="BJetGood") ]
+        "SingleEle_1b" : [ get_nElectron(1, coll="ElectronGood"), get_nBtagMin(1, coll="BJetGood") ],
+        "SingleEle_2b" : [ get_nElectron(1, coll="ElectronGood"), get_nBtagMin(2, coll="BJetGood") ],
+        "SingleEle_3b" : [ get_nElectron(1, coll="ElectronGood"), get_nBtagMin(3, coll="BJetGood") ],
+        "SingleEle_4b" : [ get_nElectron(1, coll="ElectronGood"), get_nBtagMin(4, coll="BJetGood") ],
+        "SingleMuon_1b" : [ get_nMuon(1, coll="MuonGood"), get_nBtagMin(1, coll="BJetGood") ],
+        "SingleMuon_2b" : [ get_nMuon(1, coll="MuonGood"), get_nBtagMin(2, coll="BJetGood") ],
+        "SingleMuon_3b" : [ get_nMuon(1, coll="MuonGood"), get_nBtagMin(3, coll="BJetGood") ],
+        "SingleMuon_4b" : [ get_nMuon(1, coll="MuonGood"), get_nBtagMin(4, coll="BJetGood") ],
+        "SingleEle_1b_btagcalibrated" : [ get_nElectron(1, coll="ElectronGood"), get_nBtagMin(1, coll="BJetGood") ],
+        "SingleEle_2b_btagcalibrated" : [ get_nElectron(1, coll="ElectronGood"), get_nBtagMin(2, coll="BJetGood") ],
+        "SingleEle_3b_btagcalibrated" : [ get_nElectron(1, coll="ElectronGood"), get_nBtagMin(3, coll="BJetGood") ],
+        "SingleEle_4b_btagcalibrated" : [ get_nElectron(1, coll="ElectronGood"), get_nBtagMin(4, coll="BJetGood") ],
+        "SingleMuon_1b_btagcalibrated" : [ get_nMuon(1, coll="MuonGood"), get_nBtagMin(1, coll="BJetGood") ],
+        "SingleMuon_2b_btagcalibrated" : [ get_nMuon(1, coll="MuonGood"), get_nBtagMin(2, coll="BJetGood") ],
+        "SingleMuon_3b_btagcalibrated" : [ get_nMuon(1, coll="MuonGood"), get_nBtagMin(3, coll="BJetGood") ],
+        "SingleMuon_4b_btagcalibrated" : [ get_nMuon(1, coll="MuonGood"), get_nBtagMin(4, coll="BJetGood") ]
     },
 
     
@@ -118,8 +115,8 @@ cfg =  {
                                 "sf_ele_reco", "sf_ele_id",
                                 "sf_mu_id", "sf_mu_iso", "sf_mu_trigger",
                                 "sf_jet_puId"
-                              ] + [ f"sf_btag_{b}" for b in btag_variations["2018"]]
-                              + [f"sf_ele_trigger_{v}" for v in sf_ele_trigger_variations["2018"]],
+                              ] + [ f"sf_btag_{b}" for b in btag_variations["2017"]]
+                              + [f"sf_ele_trigger_{v}" for v in sf_ele_trigger_variations["2017"]],
                 "bycategory" : {
 
                 }
