d1c05a1bbb4837fbd950aec3e8b36208ee33ec1d
Davide Valsecchi <davide.valsecchi@cern.ch>
2023-05-18 14:32:30 +0200

    Split lxplus and infn image

--- .gitlab/setup_docker.sh
+++ .gitlab/setup_docker.sh
@@ -6,7 +6,12 @@ pwd
 
 echo "======================================="
 echo "Installing PocketCoffea"
+echo "on cluster environment: "$1
 echo "Current time:" $(date)
 echo "======================================="
 
-pip3 install . 
+pip3 install .
+
+if [[ $1 == "lxplus" ]]; then
+    pip install --upgrade dask-lxplus
+fi
