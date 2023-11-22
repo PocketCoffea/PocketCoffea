10d17eeb6d05afae050d6c3980a79d0acdc529f0
Andrey Pozdnyakov <andreypz@gmail.com>
2023-11-22 17:22:18 +0100

    simplify plotting script parameters

--- scripts/plot/make_plots.py
+++ scripts/plot/make_plots.py
@@ -10,29 +10,41 @@ from pocket_coffea.utils.plot_utils import PlotManager
 from pocket_coffea.parameters import defaults
 
 parser = argparse.ArgumentParser(description='Plot histograms from coffea file')
-parser.add_argument('--cfg', help='YAML file with all the analysis parameters', required=True)
+parser.add_argument('input_dir', help='Directory with cofea files and parameters', type=str)
+parser.add_argument('--cfg', help='YAML file with all the analysis parameters', required=False)
 parser.add_argument('-op', '--overwrite_parameters', type=str, nargs="+", default=None, help='YAML file with plotting parameters to overwrite default parameters', required=False)
-parser.add_argument("-o", "--outputdir", required=True, type=str, help="Output folder")
-parser.add_argument("-i", "--inputfile", required=True, type=str, help="Input file")
-parser.add_argument('-j', '--workers', type=int, default=1, help='Number of parallel workers to use for plotting', required=False)
+parser.add_argument("-o", "--outputdir", required=False, type=str, help="Output folder")
+parser.add_argument("-i", "--inputfile", required=False, type=str, help="Input file")
+parser.add_argument('-j', '--workers', type=int, default=4, help='Number of parallel workers to use for plotting', required=False)
 parser.add_argument('-oc', '--only_cat', type=str, nargs="+", help='Filter categories with string', required=False)
 parser.add_argument('-os', '--only_syst', type=str, nargs="+", help='Filter systematics with a list of strings', required=False)
 parser.add_argument('-e', '--exclude_hist', type=str, nargs="+", default=None, help='Exclude histograms with a list of strings', required=False)
 parser.add_argument('--split_systematics', action='store_true', help='Split systematic uncertainties in the ratio plot', required=False)
 parser.add_argument('--partial_unc_band', action='store_true', help='Plot only the partial uncertainty band corresponding to the systematics specified as the argument `only_syst`', required=False)
-parser.add_argument('--overwrite', action='store_true', help='Overwrite plots in output folder', required=False)
+parser.add_argument('--overwrite', '--over', action='store_true', help='Overwrite plots in output folder', required=False)
 parser.add_argument('--log', action='store_true', help='Set y-axis scale to log', required=False)
 parser.add_argument('--density', action='store_true', help='Set density parameter to have a normalized plot', required=False)
 
 args = parser.parse_args()
 
 print("Loading the configuration file...")
+print(args)
+
+# Using the input_dir obtain the config files and coffea file (if not set with argparse):
+if args.cfg==None:
+    args.cfg = args.input_dir+"/parameters_dump.yaml"
+if args.inputfile==None:
+    args.inputfile = args.input_dir+"/output_all.coffea"
+if args.outputdir==None:
+    args.outputdir = args.input_dir+"/plots/"
 
 # Load yaml file with OmegaConf
+
 if args.cfg[-5:] == ".yaml":
     parameters_dump = OmegaConf.load(args.cfg)
 else:
     raise Exception("The input file format is not valid. The config file should be a in .yaml format.")
+
 
 # Overwrite plotting parameters
 if args.overwrite_parameters == parser.get_default("overwrite_parameters"):
