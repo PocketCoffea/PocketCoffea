bc5b12b5ce98b01370b33faeb866cb4f924c08d5
Davide Valsecchi <davide.valsecchi@cern.ch>
2024-01-24 16:09:59 +0100

    Added hadd skimmed files to cli

--- pocket_coffea/__init__.py
+++ pocket_coffea/__init__.py
@@ -6,6 +6,6 @@ PocketCoffea: Configurable analysis framework based on Coffea for CMS NanoAOD ev
 
 from __future__ import annotations
 
-__version__ = "0.1.0"
+__version__ = "0.8.0"
 
 __all__ = ("__version__",)
--- pocket_coffea/scripts/hadd_skimmed_files.py
+++ pocket_coffea/scripts/hadd_skimmed_files.py
@@ -6,104 +6,111 @@ from collections import defaultdict
 from coffea.util import load
 import subprocess
 import json
+import click
 
-parser = argparse.ArgumentParser(description='Hadd skimmed files')
-# Inputs
-parser.add_argument(
+@click.command()
+@click.option(
     '-fl',
     '--files-list',
     required=True,
     type=str,
     help='Parquet file containing the skimmed files metadata',
 )
-parser.add_argument("-o", "--outputdir", required=False, type=str, help="Output folder")
-parser.add_argument(
-    "--only-datasets", nargs="+", type=str, help="Restring list of datasets"
+@click.option("-o", "--outputdir", type=str, help="Output folder")
+@click.option(
+    "--only-datasets", 
+    type=str, 
+    multiple=True, 
+    help="Restring list of datasets"
 )
-parser.add_argument("-f", "--files", type=int, help="Limit number of files")
-parser.add_argument("-e", "--events", type=int, help="Limit number of files")
-parser.add_argument(
+@click.option("-f", "--files", type=int, help="Limit number of files")
+@click.option("-e", "--events", type=int, help="Limit number of files")
+@click.option(
     "-s",
     "--scaleout",
     type=int,
     help="Number of threads to process the hadd.",
     default=2,
 )
-parser.add_argument("--overwrite", action="store_true", help="Overwrite files")
-parser.add_argument(
-    "--dry", action="store_true", help="Do not execute hadd, save metadata"
-)
-args = parser.parse_args()
-
-df = load(args.files_list)
-
-workload = []
-groups_metadata = {}
-
-for dataset in df["skimmed_files"].keys():
-    if args.only_datasets and dataset not in args.only_datasets:
-        continue
-    groups_metadata[dataset] = defaultdict(dict)
-    nevents_tot = 0
-    nfiles = 0
-    group = []
-    ngroup = 1
-    for file, nevents in zip(
-        df["skimmed_files"][dataset], df["nskimmed_files"][dataset]
-    ):
-        if (args.files and (nfiles + 1) > args.files) or (
-            args.events and (nevents_tot + nevents) > args.events
+@click.option("--overwrite", is_flag=True, help="Overwrite files")
+@click.option("--dry", is_flag=True, help="Do not execute hadd, save metadata")
+def hadd_skimmed_files(files_list, outputdir, only_datasets, files, events, scaleout, overwrite, dry):
+    '''
+    Regroup skimmed datasets by joining different files (like hadd for ROOT files). 
+    '''
+    df = load(files_list)
+    workload = []
+    groups_metadata = {}
+    
+    for dataset in df["skimmed_files"].keys():
+        if only_datasets and dataset not in only_datasets:
+            continue
+        groups_metadata[dataset] = defaultdict(dict)
+        nevents_tot = 0
+        nfiles = 0
+        group = []
+        ngroup = 1
+        for file, nevents in zip(
+            df["skimmed_files"][dataset], df["nskimmed_files"][dataset]
         ):
-            outfile = f"{args.outputdir}/{dataset}/{dataset}_{ngroup}.root"
+            if (files and (nfiles + 1) > files) or (
+                events and (nevents_tot + nevents) > events
+            ):
+                outfile = f"{outputdir}/{dataset}/{dataset}_{ngroup}.root"
+                workload.append((outfile, group[:]))
+                groups_metadata[dataset]["files"][outfile] = group[:]
+                group.clear()
+                ngroup += 1
+                nfiles = 0
+                nevents_tot = 0
+            else:
+                group.append(file)
+                nfiles += 1
+                nevents_tot += nevents
+
+        # add last group
+        if len(group):
+            outfile = f"{outputdir}/{dataset}/{dataset}_{ngroup}.root"
             workload.append((outfile, group[:]))
             groups_metadata[dataset]["files"][outfile] = group[:]
-            group.clear()
-            ngroup += 1
-            nfiles = 0
-            nevents_tot = 0
-        else:
-            group.append(file)
-            nfiles += 1
-            nevents_tot += nevents
 
-    # add last group
-    if len(group):
-        outfile = f"{args.outputdir}/{dataset}/{dataset}_{ngroup}.root"
-        workload.append((outfile, group[:]))
-        groups_metadata[dataset]["files"][outfile] = group[:]
+    print(f"We will hadd {len(workload)} groups of files.")
+    print("Samples:", groups_metadata.keys())
 
-print(f"We will hadd {len(workload)} groups of files.")
-print("Samples:", groups_metadata.keys())
-
-
-def do_hadd(group):
-    try:
-        print("Running: ", group[0])
-        if args.overwrite:
-            subprocess.run(["hadd", "-f", group[0], *group[1]], check=True)
-        else:
-            subprocess.run(["hadd", group[0], *group[1]], check=True)
-        return group[0], 0
-    except subprocess.CalledProcessError as e:
-        print("Error producing group: ", group[0])
-        print(e.stderr)
-        return group[0], 1
+    def do_hadd(group):
+        try:
+            print("Running: ", group[0])
+            if overwrite:
+                subprocess.run(["hadd", "-f", group[0], *group[1]], check=True)
+            else:
+                subprocess.run(["hadd", group[0], *group[1]], check=True)
+            return group[0], 0
+        except subprocess.CalledProcessError as e:
+            print("Error producing group: ", group[0])
+            print(e.stderr)
+            return group[0], 1
 
 
-if not args.dry:
-    p = Pool(args.scaleout)
-    results = p.map(do_hadd, workload)
+    if not dry:
+        p = Pool(scaleout)
+        results = p.map(do_hadd, workload)
 
-    print("\n\n\n")
-    for group, r in results:
-        if r != 0:
-            print("#### Failed hadd: ", group)
+        print("\n\n\n")
+        for group, r in results:
+            if r != 0:
+                print("#### Failed hadd: ", group)
 
-json.dump(groups_metadata, open("./hadd.json", "w"), indent=2)
+    json.dump(groups_metadata, open("hadd.json", "w"), indent=2)
 
-# Now saving the dataset definition file
-dataset_definition = {
-    s: {"files": list(d['files'].keys())} for s, d in groups_metadata.items()
-}
+    # Now saving the dataset definition file
+    dataset_definition = {
+        s: {"files": list(d['files'].keys())} for s, d in groups_metadata.items()
+    }
 
-json.dump(dataset_definition, open("./skimmed_dataset_definition.json", "w"), indent=2)
+    json.dump(dataset_definition, open("skimmed_dataset_definition.json", "w"), indent=2)
+
+    print("DONE!")
+
+
+if __name__ == "__main__":
+    hadd_skimmed_files()
