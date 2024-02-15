bf634e94bb271560bfe13e4ab2834424eaf94528
Davide Valsecchi <davide.valsecchi@cern.ch>
2024-02-15 19:50:03 +0100

    Fixing #133

--- pocket_coffea/lib/jets.py
+++ pocket_coffea/lib/jets.py
@@ -216,14 +216,15 @@ def jet_selection(events, jet_type, params, leptons_collection=""):
         dR_jets_lep = jets.metric_table(events[leptons_collection])
         mask_lepton_cleaning = ak.prod(dR_jets_lep > cuts["dr_lepton"], axis=2) == 1
 
-    # Selection on PUid. Only available in Run2 UL, thus we need to determine which sample we run over:
-    if events.metadata["year"] in ['2016_PreVFP', '2016_PostVFP','2017','2018']:
-        mask_jetpuid = (jets.puId >= cuts["puId"]["value"]) | (
-            jets.pt >= cuts["puId"]["maxpt"]
-        )
-    else:
-        mask_jetpuid = True
     if jet_type == "Jet":
+        # Selection on PUid. Only available in Run2 UL, thus we need to determine which sample we run over:
+        if events.metadata["year"] in ['2016_PreVFP', '2016_PostVFP','2017','2018']:
+            mask_jetpuid = (jets.puId >= cuts["puId"]["value"]) | (
+                jets.pt >= cuts["puId"]["maxpt"]
+            )
+        else:
+            mask_jetpuid = True
+  
         mask_good_jets = mask_presel & mask_lepton_cleaning & mask_jetpuid
 
     elif jet_type == "FatJet":
