2cef2ac76327b89e71496a56573ebf2ba9cf890f
Andrey Pozdnyakov <andreypz@gmail.com>
2024-04-23 17:26:17 +0200

    merge output script with argparse

---
+++ pocket_coffea/scripts/merge_outputs_argparse.py
@@ -0,0 +1,21 @@
+from coffea.util import load, save
+from coffea.processor import accumulate 
+
+import argparse
+parser = argparse.ArgumentParser(description='Merge datasets from different coffea outputs')
+# Inputs
+parser.add_argument('-i','--inputfiles', required=True, type=str, nargs="+",
+                    help='List of coffea input files')
+parser.add_argument("-o", "--outputfile", required=True, type=str,
+                    help="Output file")
+args = parser.parse_args()
+
+def merge_outputs(inputfiles, outputfile):
+    files = [load(f) for f in inputfiles]
+
+    out = accumulate(files)
+    save(out, outputfile)
+
+
+if __name__ == "__main__":
+    merge_outputs(args.inputfiles, args.outputfile)
