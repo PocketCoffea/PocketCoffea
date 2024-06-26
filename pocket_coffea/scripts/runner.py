ce20a11cb88a955dd7e905002494c4b3b6de9543
Davide Valsecchi <davide.valsecchi@cern.ch>
2024-06-26 13:41:31 +0200

    Filtering by dataset in the runner script

--- pocket_coffea/scripts/runner.py
+++ pocket_coffea/scripts/runner.py
@@ -49,10 +49,12 @@ def load_config(cfg, outputdir):
 @click.option("-ll","--loglevel", type=str, help="Console logging level", default="INFO" )
 @click.option("-ps","--process-separately", is_flag=True, help="Process each dataset separately", default=False )
 @click.option("--executor-custom-setup", type=str, help="Python module to be loaded as custom executor setup")
+@click.option("--filter-years", type=str, help="Filter the data taking period of the datasets to be processed (comma separated list)")
 
 def run(cfg,  custom_run_options, outputdir, test, limit_files,
            limit_chunks, executor, scaleout, chunksize,
-           queue, loglevel, process_separately, executor_custom_setup):
+           queue, loglevel, process_separately, executor_custom_setup,
+           filter_years):
     '''Run an analysis on NanoAOD files using PocketCoffea processors'''
 
     # Setting up the output dir
@@ -168,10 +170,23 @@ def run(cfg,  custom_run_options, outputdir, test, limit_files,
     # Instantiate the executor
     executor = executor_factory.get()
 
+    # Filter on the fly the fileset to process by datataking period
+    filesets_to_run = {}
+    filter_years = filter_years.split(",") if filter_years else None
+    if filter_years:
+        for fileset_name, fileset in config.filesets.items():
+            if fileset["metadata"]["year"] in filter_years:
+                filesets_to_run[fileset_name] = fileset
+    else:
+        filesets_to_run = config.filesets
+
+    if len(filesets_to_run) == 0:
+        print("No datasets to process, closing")
+        exit(1)
+        
     if not process_separately:
         # Running on all datasets at once
-        fileset = config.filesets
-        logging.info(f"Working on samples: {list(fileset.keys())}")
+        logging.info(f"Working on samples: {list(filesets_to_run.keys())}")
 
         run = Runner(
             executor=executor,
@@ -181,7 +196,7 @@ def run(cfg,  custom_run_options, outputdir, test, limit_files,
             schema=processor.NanoAODSchema,
             format="root",
         )
-        output = run(fileset, treename="Events",
+        output = run(filesets_to_run, treename="Events",
                      processor_instance=config.processor_instance)
 
         print(f"Saving output to {outfile.format('all')}")
@@ -189,11 +204,11 @@ def run(cfg,  custom_run_options, outputdir, test, limit_files,
 
     else:
         # Running separately on each dataset
-        for sample, files in config.filesets.items():
+        for sample, files in config.filesets_to_run.items():
             print(f"Working on sample: {sample}")
             logging.info(f"Working on sample: {sample}")
             
-            fileset = {sample:files}
+            fileset_ = {sample:files}
 
             n_events_tot = int(files["metadata"]["nevents"])
             n_workers_max = n_events_tot / run_options["chunksize"]
@@ -214,7 +229,7 @@ def run(cfg,  custom_run_options, outputdir, test, limit_files,
                 schema=processor.NanoAODSchema,
                 format="root",
             )
-            output = run(fileset, treename="Events",
+            output = run(fileset_, treename="Events",
                          processor_instance=config.processor_instance)
             print(f"Saving output to {outfile.format(sample)}")
             save(output, outfile.format(sample))
