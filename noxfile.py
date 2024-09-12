f545c7c3bff6d1cd2c1bfeaf18a07b8c0647b73f
Felix Philipp Zinn <felix.zinn@rwth-aachen.de>
2024-09-12 12:15:37 +0200

    fix module name in pylint nox config

--- noxfile.py
+++ noxfile.py
@@ -32,7 +32,7 @@ def pylint(session: nox.Session) -> None:
     # This needs to be installed into the package environment, and is slower
     # than a pre-commit check
     session.install(".", "pylint")
-    session.run("pylint", "src", *session.posargs)
+    session.run("pylint", "pocket_coffea", *session.posargs)
 
 
 @nox.session(reuse_venv=True)
