52666b4a27551a7f02a43da43d0aac61b7c870cf
Matteo Marchegiani <matmarcheg@gmail.com>
2024-09-27 09:18:51 +0200

    Update btag SF calibration

--- pocket_coffea/lib/weights/common/common.py
+++ pocket_coffea/lib/weights/common/common.py
@@ -1,5 +1,6 @@
 from ..weights import WeightWrapper, WeightLambda, WeightData, WeightDataMultiVariation
 import numpy as np
+import awkward as ak
 
 from pocket_coffea.lib.scale_factors import (
     sf_ele_reco,
@@ -196,7 +197,7 @@ class SF_btag(WeightWrapper):
 
 class SF_btag_calib(WeightWrapper):
     name = "sf_btag_calib"
-    has_variations = True
+    has_variations = False
 
     def __init__(self, params, metadata):
         super().__init__(params, metadata)
@@ -213,12 +214,12 @@ class SF_btag_calib(WeightWrapper):
                             )
         return WeightData(
             name = self.name,
-            nominal = out[0],
-            up = out[1],
-            down = out[2]
+            nominal = out,#[0],
+            #up = out[1],
+            #down = out[2]
             )
 
-    
+
 ########################################
 # Ctag scale factors have weights depending on the shape_variation
 
