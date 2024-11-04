5366d6ad5c3f4192ca4cbc02021dba637c77a5c6
Philipp Nattland <philipp.nattland@rwth-aachen.de>
2024-11-04 14:55:34 +0100

    fixing electron smearing random seed to be positive

--- pocket_coffea/workflows/base.py
+++ pocket_coffea/workflows/base.py
@@ -715,7 +715,7 @@ class BaseProcessorABC(processor.ProcessorABC, ABC):
                     ssfile = self.params.lepton_scale_factors["electron_sf"]["JSONfiles"][self._year]["fileSS"]
                     # Apply smearing on MC, scaling on Data
                     if self._isMC:
-                        seed = hash(self.events.metadata['fileuuid'])+self.events.metadata['entrystart']
+                        seed = abs(hash(self.events.metadata['fileuuid'])+self.events.metadata['entrystart'])
                         ele_pt_smeared = get_ele_smeared(self.events["Electron"], ssfile, self._isMC, nominal=True, seed=seed)
                         self.events["Electron"] = ak.with_field(
                             self.events["Electron"], ele_pt_smeared["nominal"], "pt"
