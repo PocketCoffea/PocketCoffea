6777a472135e01c71c62d6e2c8fc285fd38a66df
Matteo Marchegiani <matmarcheg@gmail.com>
2024-11-05 00:44:42 +0100

    Compare dict values types before merging outputs

--- pocket_coffea/scripts/merge_outputs.py
+++ pocket_coffea/scripts/merge_outputs.py
@@ -2,6 +2,7 @@ from coffea.util import load, save
 from coffea.processor import accumulate 
 import click
 from rich import print
+from pocket_coffea.utils.filter_output import compare_dict_types
 
 @click.command()
 @click.argument(
@@ -22,6 +23,15 @@ def merge_outputs(inputfiles, outputfile):
     '''Merge coffea output files'''
     print(f"[blue]Merging files into {outputfile}[/]")
     print(sorted(inputfiles))
+    type_mismatches = []
+    f0 = inputfiles[0]
+    for f in inputfiles[1:]:
+        print(f"Comparing {f0} and {f}")
+        type_mismatch_found = compare_dict_types(load(f0), load(f))
+        type_mismatches.append(type_mismatch_found)
+    if any(type_mismatches):
+        raise TypeError("Type mismatch found between the values of the input dictionaries. Please check the input files.")
+
     out = accumulate([load(f) for f in inputfiles])
     save(out, outputfile)
     print(f"[green]Output saved to {outputfile}")
