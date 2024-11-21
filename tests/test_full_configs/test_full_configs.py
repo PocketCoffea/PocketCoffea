c26e95dee4b78194daf9f5c786bd0bf6f1d49325
Davide Valsecchi <davide.valsecchi@cern.ch>
2024-11-21 19:05:44 +0100

    fix: test

--- tests/test_full_configs/test_full_configs.py
+++ tests/test_full_configs/test_full_configs.py
@@ -43,7 +43,7 @@ def test_new_weights(base_path: Path, monkeypatch: pytest.MonkeyPatch, tmp_path_
     run_options = defaults.get_default_run_options()["general"]
     run_options["limit-files"] = 1
     run_options["limit-chunks"] = 1
-    run_options["chunksize"] = 500
+    run_options["chunksize"] = 10000
     config.filter_dataset(run_options["limit-files"])
 
     executor_factory = executors_lib.get_executor_factory("iterative",
