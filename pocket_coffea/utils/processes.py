5df8a1b61a02cf7c4e5566814acacae1f0048a18
Matteo Marchegiani <matmarcheg@gmail.com>
2024-12-12 14:11:34 +0100

    Allow samples to be iterable

--- pocket_coffea/utils/processes.py
+++ pocket_coffea/utils/processes.py
@@ -1,14 +1,14 @@
 """Physical Processes as Dataclasses and Utilities"""
 
+from typing import Iterable
 from dataclasses import dataclass
-
 
 @dataclass
 class Process:
     """Class to store information of a physical process"""
 
     name: str
-    samples: list
+    samples: Iterable
     is_signal: bool
     is_data: bool = False
     label: str = None
@@ -16,6 +16,11 @@ class Process:
     def __post_init__(self):
         if not self.label:
             self.label = self.name
+
+    def __post_init_post_parse__(self):
+        '''Ensure samples is a list'''
+        if not isinstance(self.samples, list):
+            self.samples = list(self.samples)
 
 
 @dataclass
