0b304371971a83e2c5eb93d3513a278f4d736f64
Matteo Marchegiani <matmarcheg@gmail.com>
2025-02-12 13:50:48 +0100

    Rebin combine prefit histograms on the fly

--- pocket_coffea/utils/datacard.py
+++ pocket_coffea/utils/datacard.py
@@ -25,6 +25,7 @@ class Datacard(Processes, Systematics):
         data_processes: list[Process] = None,
         systematics: list[SystematicUncertainty] = None,
         mcstat: bool = True,
+        bins_edges: list[float] = None,
         bin_prefix: str = None,
         bin_suffix: str = None,
     ) -> None:
@@ -51,6 +52,7 @@ class Datacard(Processes, Systematics):
         self.data_processes = data_processes
         self.systematics = systematics
         self.mcstat = mcstat
+        self.bins_edges = bins_edges
         self.years = years
         self.category = category
         self.bin_prefix = bin_prefix
@@ -65,6 +67,9 @@ class Datacard(Processes, Systematics):
             self.hist_mode = 1
         if self.has_data and (len(self.data_processes) != 1):
             raise NotImplementedError("Only one data process is supported.")
+        # If bin edges are passed, rebin histograms
+        if self.bins_edges:
+            self.histograms = self.rebin_hist()
 
         # assign IDs to processes
         self.process_id = {}
@@ -238,6 +243,58 @@ class Datacard(Processes, Systematics):
                             raise ValueError(error_message)
                         else:
                             print(error_message)
+
+    # Taken from https://gist.github.com/kdlong/d697ee691c696724fc656186c25f8814
+    def rebin_hist(self):
+        edges = self.bins_edges
+        h_dict_new = {}
+        for sample, dict_dataset in self.histograms.items():
+            h_dict_new[sample] = {}
+            for dataset, h in dict_dataset.items():
+                axis_name = h.axes[-1].name
+                if type(edges) == int:
+                    return h[{axis_name : hist.rebin(edges)}]
+
+                ax = h.axes[axis_name]
+                ax_idx = [a.name for a in h.axes].index(axis_name)
+                if not all([np.isclose(x, ax.edges).any() for x in edges]):
+                    raise ValueError(f"Cannot rebin histogram due to incompatible edges for axis '{ax.name}'\n"
+                                        f"Edges of histogram are {ax.edges}, requested rebinning to {edges}")
+
+                # If you rebin to a subset of initial range, keep the overflow and underflow
+                overflow = ax.traits.overflow or (edges[-1] < ax.edges[-1] and not np.isclose(edges[-1], ax.edges[-1]))
+                underflow = ax.traits.underflow or (edges[0] > ax.edges[0] and not np.isclose(edges[0], ax.edges[0]))
+                flow = overflow or underflow
+                new_ax = hist.axis.Variable(edges, name=ax.name, overflow=overflow, underflow=underflow)
+                axes = list(h.axes)
+                axes[ax_idx] = new_ax
+
+                hnew = hist.Hist(*axes, name=h.name, storage=h._storage_type())
+
+                # Offset from bin edge to avoid numeric issues
+                offset = 0.5*np.min(ax.edges[1:]-ax.edges[:-1])
+                edges_eval = edges+offset
+                edge_idx = ax.index(edges_eval)
+                # Avoid going outside the range, reduceat will add the last index anyway
+                if edge_idx[-1] == ax.size+ax.traits.overflow:
+                    edge_idx = edge_idx[:-1]
+
+                if underflow:
+                    # Only if the original axis had an underflow should you offset
+                    if ax.traits.underflow:
+                        edge_idx += 1
+                    edge_idx = np.insert(edge_idx, 0, 0)
+
+                # Take is used because reduceat sums i:len(array) for the last entry, in the case
+                # where the final bin isn't the same between the initial and rebinned histogram, you
+                # want to drop this value. Add tolerance of 1/2 min bin width to avoid numeric issues
+                hnew.values(flow=flow)[...] = np.add.reduceat(h.values(flow=flow), edge_idx, 
+                        axis=ax_idx).take(indices=range(new_ax.size+underflow+overflow), axis=ax_idx)
+                if hnew._storage_type() == hist.storage.Weight():
+                    hnew.variances(flow=flow)[...] = np.add.reduceat(h.variances(flow=flow), edge_idx, 
+                            axis=ax_idx).take(indices=range(new_ax.size+underflow+overflow), axis=ax_idx)
+                h_dict_new[sample][dataset] = hnew
+        return h_dict_new
 
     def rearrange_histograms(
         self,
@@ -564,15 +621,14 @@ def create_scripts(
     directory: str,
     card_name: str = "datacard_combined.txt",
     workspace_name : str = "workspace.root",
-    channel_masks : list[str] = None,
+    categories_masked : list[str] = None,
     suffix: str = None,
     ) -> None:
     """
     Write the bash scripts to run the fit with CMS Combine Tool."""
 
     # Save fit scripts
-    freezeParameters = ["SF_diboson", "SF_singletop", "SF_tt_dilepton", "SF_ttv", "SF_vjets", "r"]
-    freezeParameters_asimov = ["SF_diboson", "SF_singletop", "SF_tt_dilepton", "SF_ttv", "SF_vjets"]
+    freezeParameters = ["r"]
 
     args = {
         "run_MultiDimFit.sh": [[
@@ -607,7 +663,6 @@ def create_scripts(
             "combine -M MultiDimFit",
             f"-d {workspace_name}",
             "-n .asimov_fit",
-            f"--freezeParameters {','.join(freezeParameters_asimov)}",
             "-t -1 --toysFrequentist --expectSignal=1",
             "--cminDefaultMinimizerStrategy 2 --robustFit=1",
             "--saveWorkspace -v 2",
@@ -616,7 +671,6 @@ def create_scripts(
             "combine -M FitDiagnostics",
             f"-d {workspace_name}",
             "-n .asimov_fit",
-            f"--freezeParameters {','.join(freezeParameters_asimov)}",
             "-t -1 --toysFrequentist --expectSignal=1",
             "--cminDefaultMinimizerStrategy 2 --robustFit=1",
             "--saveWorkspace -v 2",
@@ -625,7 +679,6 @@ def create_scripts(
             "combine -M MultiDimFit",
             f"-d {workspace_name}",
             "-n .asimov_scan1d",
-            f"--freezeParameters {','.join(freezeParameters_asimov)}",
             "-t -1 --toysFrequentist --expectSignal=1",
             "--cminDefaultMinimizerStrategy 2 --robustFit=1",
             "--saveWorkspace",
@@ -653,18 +706,35 @@ def create_scripts(
             ["combine -M MultiDimFit",
             f"-d {workspace_name}",
             "-n .covariance_matrix",
-            f"--freezeParameters {','.join(freezeParameters_asimov)}",
             "-t -1 --toysFrequentist --expectSignal=1",
             "--cminDefaultMinimizerStrategy 2 --robustFit=1",
             "-v 2 --rMin -5 --rMax 5 --robustHesse=1 --robustHesseSave 1 --saveFitResult"]
         ]
     }
-    if channel_masks:
-        for cat in channel_masks:
-            args[f"run_MultiDimFit_mask_{cat}.sh"][0] = args["run_MultiDimFit.sh"][0] + [f"--setParameters mask_{cat}=1"]
-            args[f"run_FitDiagnostics_mask_{cat}.sh"][0] = args["run_FitDiagnostics.sh"][0] + [f"--setParameters mask_{cat}=1"]
-            args[f"run_MultiDimFit_mask_{cat}.sh"][0][2] = "-n .snapshot_CR_CR_ttlf"
-            args[f"run_FitDiagnostics_mask_{cat}.sh"][0][2] = "-n .snapshot_CR_CR_ttlf"
+    if categories_masked:
+        args.update({
+            f"run_MultiDimFit_mask_{'_'.join(categories_masked)}.sh" : [[
+                "combine -M MultiDimFit",
+                f"-d {workspace_name}",
+                f"-n .snapshot_{'_'.join(categories_masked)}",
+                f"--freezeParameters {','.join(freezeParameters)}",
+                "--cminDefaultMinimizerStrategy 2 --robustFit=1",
+                "--saveWorkspace",
+                f"--setParameters {','.join([f'mask_{cat}=1' for cat in categories_masked ])}"
+            ]],
+            f"run_FitDiagnostics_mask_{'_'.join(categories_masked)}.sh" : [[
+                "combine -M FitDiagnostics",
+                f"-d {workspace_name}",
+                f"-n .snapshot_{'_'.join(categories_masked)}",
+                f"--freezeParameters {','.join(freezeParameters)}",
+                "--cminDefaultMinimizerStrategy 2 --robustFit=1",
+                "--saveWorkspace",
+                "--saveShapes",
+                "--saveWithUncertainties",
+                f"--setParameters {','.join([f'mask_{cat}=1' for cat in categories_masked ])}"
+            ]],
+        })
+
     scripts = {}
     for path, lines in args.items():
         scripts[path] = [f"{' '.join(l)}\n" for l in lines]
