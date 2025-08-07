a44ddbc2e02fc614fea81a6010896242000bda65
matteomalucchi <matteomalucchi13@gmail.com>
2025-08-07 15:34:30 +0200

    first changes

--- pocket_coffea/lib/calibrators/common/common.py
+++ pocket_coffea/lib/calibrators/common/common.py
@@ -108,6 +108,109 @@ class JetsCalibrator(Calibrator):
             
         return out
 
+class JetsPtRegressionCalibrator(Calibrator):
+    """
+    """
+    name = "jet_pt_reg_calibration"
+    has_variations = True
+    isMC_only = False
+    calibrated_collections = ["Jet"]
+
+
+    def __init__(self, params, metadata, jme_factory, **kwargs):
+        super().__init__(params, metadata)
+        self.jme_factory = jme_factory
+        self.jet_calib_param = self.params.jets_calibration
+        self.caches = [] 
+        self._year = self.metadata["year"]
+        self.jets_calibrated = {}
+        self.jets_calibrated_types = [ ]
+
+    def initialize(self, events):
+        # Load the calibration of each jet type requested by the parameters
+        for jet_type, jet_coll_name in self.jet_calib_param.collection[self._year].items():
+            # Check if the collection is enables in the parameters
+            if self.metadata["isMC"]:
+                if self.jet_calib_param.apply_jec_MC[self._year][jet_type] == False:
+                    # If the collection is not enabled, we skip it
+                    continue
+            else:
+                if self.jet_calib_param.apply_jec_Data[self._year][jet_type] == False:
+                    # If the collection is not enabled, we skip it
+                    continue
+            cache = cachetools.Cache(np.inf)
+            self.caches.append(cache)
+            self.jets_calibrated[jet_coll_name] = jet_correction(
+                params=self.params,
+                events=events,
+                jets=events[jet_coll_name],
+                factory=self.jme_factory,
+                jet_type = jet_type,
+                chunk_metadata={
+                    "year": self.metadata["year"],
+                    "isMC": self.metadata["isMC"],
+                    "era": self.metadata["era"] if "era" in self.metadata else None,
+                },
+                cache=cache
+            )
+            # Add to the list of the types calibrated
+            self.jets_calibrated_types.append(jet_type)
+
+        # Prepare the list of available variations
+        # For this we just read from the parameters
+        available_jet_variations = []
+        for jet_type in self.jet_calib_param.collection[self._year].keys():
+            if jet_type not in self.jets_calibrated_types:
+                # If the jet type is not calibrated, we skip it
+                continue
+            if jet_type in self.jet_calib_param.variations[self._year]:
+                # If the jet type has variations, we add them to the list
+                # of variations available for this calibrator
+                for variation in self.jet_calib_param.variations[self._year][jet_type]:
+                    available_jet_variations +=[
+                        f"{jet_type}_{variation}Up",
+                        f"{jet_type}_{variation}Down"
+                    ]
+                    # we want to vary independently each jet type
+        self._variations = list(sorted(set(available_jet_variations)))  # remove duplicates
+
+
+    def calibrate(self, events, orig_colls, variation, already_applied_calibrators=None):
+        # The values have been already calculated in the initialize method
+        # We just need to apply the corrections to the events
+        out = {}
+        if variation == "nominal" or variation not in self._variations:
+            # If the variation is nominal or not in the list of variations, we return the nominal values
+            for jet_coll_name, jets in self.jets_calibrated.items():
+                out[jet_coll_name] = jets
+        else:
+            # get the jet type from the variation name
+            variation_parts = variation.split("_")
+            jet_type = variation_parts[0]
+            if jet_type not in self.jet_calib_param.collection[self._year]:
+                raise ValueError(f"Jet type {jet_type} not found in the parameters for year {self._year}.")
+            # get the variation type from the variation name
+            if variation.endswith("Up"):
+                variation_type = "_".join(variation_parts[1:])[:-2]  # remove 'Up'
+                direction = "up"
+            elif variation.endswith("Down"):
+                variation_type = "_".join(variation_parts[1:])[:-4]  # remove 'Down'
+                direction = "down"
+            else:
+                raise ValueError(f"JET Variation {variation} is not recognized. It should end with 'Up' or 'Down'.")
+           
+            # get the jet collection name from the parameters
+            jet_coll_name = self.jet_calib_param.collection[self._year][jet_type]
+            if jet_coll_name not in self.jets_calibrated:
+                raise ValueError(f"Jet collection {jet_coll_name} not found in the calibrated jets.")
+            # Apply the variation to the jets
+            if direction == "up":
+                out[jet_coll_name] = self.jets_calibrated[jet_coll_name][variation_type].up
+            elif direction == "down":
+                out[jet_coll_name] = self.jets_calibrated[jet_coll_name][variation_type].down
+            
+        return out
+
 ###########################################
 class METCalibrator(Calibrator):
 
