9350c757705ebd1d3007154cc3afe14c05a0c8e2
valsdav <davide.valsecchi@cern.ch>
2025-08-12 09:24:57 +0200

    bump version

--- pocket_coffea/__meta__.py
+++ pocket_coffea/__meta__.py
@@ -6,10 +6,10 @@ Meta information for the package.
 
 __author__ = "Davide Valsecchi, Matteo Marchegiani"
 __email__ = "davide.valsecchi@cern.ch"
-__copyright__ = "Copyright 2021-2024"
+__copyright__ = "Copyright 2021-2025"
 __credits__ = ["Davide Valsecchi", "Matteo Marchegiani"]
 __contact__ = "https://github.com/PocketCoffea/PocketCoffea"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "0.9.9"
+__version__ = "0.9.10"
 __all__ = []
