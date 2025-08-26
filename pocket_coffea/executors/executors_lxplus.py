fbaaf38b39921531b26df069b5e17f2ac2906675
mondalspandan <mondalspandan@yahoo.co.in>
2025-08-26 13:36:08 +0200

    Fix condor@lxplus bug that affects vanilla apptainer submissions
    Fix condor@lxplus bug that affects vanilla apptainer submissions
    
    Fix condor@lxplus bug that affects vanilla apptainer submissions

--- pocket_coffea/executors/executors_lxplus.py
+++ pocket_coffea/executors/executors_lxplus.py
@@ -179,6 +179,11 @@ class ExecutorFactoryCondorCERN(ExecutorFactoryManualABC):
         env_extras= "\n".join(env_extras_list)
 
         pythonpath = sys.prefix.rsplit('/', 1)[0]
+        runnerpath = f"{pythonpath}/pocket_coffea/scripts/runner.py"
+        if os.path.isfile(runnerpath):
+            runnercmd = "python " + runnerpath
+        else:
+            runnercmd = "pocket-coffea run"
 
         if self.run_options["split-by-category"]:
             splitcommands = '''
@@ -203,7 +208,7 @@ rm -f $JOBDIR/job_$1.idle
 echo "Starting job $1"
 touch $JOBDIR/job_$1.running
 
-python {pythonpath}/pocket_coffea/scripts/runner.py --cfg $2 -o output EXECUTOR --chunksize $4
+{runnercmd} --cfg $2 -o output EXECUTOR --chunksize $4
 # Do things only if the job is successful
 if [ $? -eq 0 ]; then
     echo 'Job successful'
