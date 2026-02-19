a30594c1c9ef4b867671e3d19ce99006003951f5
Matteo Marchegiani <matmarcheg@gmail.com>
2026-02-19 04:26:22 -0600

    Setup LPCCondorCluster parameters

--- pocket_coffea/executors/executors_lpc.py
+++ pocket_coffea/executors/executors_lpc.py
@@ -13,6 +13,43 @@ import cloudpickle
 import yaml
 from copy import deepcopy
 
+def get_worker_env(run_options,x509_path,exec_name="dask"):
+    env_worker = [
+        'export XRD_RUNFORKHANDLER=1',
+        'export MALLOC_TRIM_THRESHOLD_=0'        ,
+        ]
+    if exec_name == "dask":
+        env_worker.append('ulimit -u unlimited')
+
+    if not run_options['ignore-grid-certificate']:
+        env_worker.append(f'export X509_USER_PROXY={x509_path}')
+    
+    # Adding list of custom setup commands from user defined run options
+    if run_options.get("custom-setup-commands", None):
+        env_worker += run_options["custom-setup-commands"]
+
+    # Now checking for conda environment  conda-env:true
+    if run_options.get("conda-env", False):
+        env_worker.append(f'export PATH={os.environ["CONDA_PREFIX"]}/bin:$PATH')
+        if "CONDA_ROOT_PREFIX" in os.environ:
+            env_worker.append(f"{os.environ['CONDA_ROOT_PREFIX']} activate {os.environ['CONDA_DEFAULT_ENV']}")
+        elif "MAMBA_ROOT_PREFIX" in os.environ:
+            env_worker.append(f"{os.environ['MAMBA_ROOT_PREFIX']} activate {os.environ['CONDA_DEFAULT_ENV']}")
+        else:
+            raise Exception("CONDA prefix not found in env! Something is wrong with your conda installation if you want to use conda in the dask cluster.")
+
+    # if local-virtual-env: true the dask job is configured to pickup
+    # the local virtual environment. 
+    if run_options.get("local-virtualenv", False):
+        env_worker.append(f"source {sys.prefix}/bin/activate")
+        if exec_name == "dask":
+            env_worker.append(f"export PYTHONPATH={sys.prefix}/lib:$PYTHONPATH")
+        elif exec_name == "condor":
+            pythonpath = sys.prefix.rsplit('/', 1)[0]
+            env_worker.append(f"export PYTHONPATH={pythonpath}")
+
+    return env_worker
+
 class DaskExecutorFactory(ExecutorFactoryABC):
 
     def __init__(self, run_options, outputdir, **kwargs):
@@ -40,31 +77,34 @@ class DaskExecutorFactory(ExecutorFactoryABC):
                 f"Port '{n_port}' is already occupied on this node. Change the port or try a different machine."
             )
         # Creating a LPC Cluster, special configuration for dask-on-lpc
-        log_folder = "condor_log"
 
         ########################################################################
         # CHECK PARAMETERS FOR LPC CLUSTER
         ########################################################################
 
+        # Define log_directory using absolute path
+        log_directory = os.path.abspath(f"{self.outputdir}/condor_log")
+        print("log_directory:", log_directory)
+
         self.dask_cluster = LPCCondorCluster(
                 cores=self.run_options['cores-per-worker'],
                 memory=self.run_options['mem-per-worker'],
                 disk=self.run_options['disk-per-worker'],
-                image_type="singularity",
-                worker_image=self.run_options["worker-image"],
+                #image_type="singularity",
+                #worker_image=self.run_options["worker-image"],
                 death_timeout=self.run_options["death-timeout"],
                 scheduler_options={"port": n_port, "host": socket.gethostname()},
-                log_directory = f"{self.outputdir}/{log_folder}",
+                log_directory = log_directory,
                 # shared_temp_directory="/tmp"
-                job_extra={
-                    "log": f"{self.outputdir}/{log_folder}/dask_job_output.log",
-                    "output": f"{self.outputdir}/{log_folder}/dask_job_output.out",
-                    "error": f"{self.outputdir}/{log_folder}/dask_job_output.err",
+                job_extra_directives={
+                    "log": os.path.join(log_directory, "dask_job_output.log"),
+                    "output": os.path.join(log_directory, "dask_job_output.out"),
+                    "error": os.path.join(log_directory, "dask_job_output.err"),
                     "should_transfer_files": "Yes", #
                     "when_to_transfer_output": "ON_EXIT",
                     "+JobFlavour": f'"{self.run_options["queue"]}"'
                 },
-                env_extra=get_worker_env(self.run_options,self.x509_path,"dask"),
+                job_script_prologue=get_worker_env(self.run_options,self.x509_path,"dask"),
             )
 
         ########################################################################
@@ -83,7 +123,7 @@ class DaskExecutorFactory(ExecutorFactoryABC):
         print(">> You can connect to the Dask viewer at http://localhost:8787")
 
         # if self.run_options["performance-report"]:
-        #     self.performance_report_path = os.path.join(self.outputdir, f"{log_folder}/dask-report.html")
+        #     self.performance_report_path = os.path.join(self.outputdir, f"{log_directory}/dask-report.html")
         #     print(f"Saving performance report to {self.performance_report_path}")
         #     self.performance_report(filename=performance_report_path):
 
